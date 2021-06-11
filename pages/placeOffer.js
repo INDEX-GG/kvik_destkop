4c251b7887ae438d764b3ce5714b4bcb655d6340
Yaroslav_Makarov <slideryo@gmail.com>
2021-06-11 14:05:03 +0500

    reg form and verify states

--- pages/placeOffer.js
+++ pages/placeOffer.js
@@ -1,22 +1,40 @@
+import { Box, Container, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
-import Footer from '../components/Footer';
-import Header from '../components/Header';
 import Verify from '../components/placeOffer/Verify';
+import MainLayout from '../layout/MainLayout';
+import {useMedia} from '../hooks/useMedia';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        position: 'relative',
+        flexDirection: 'column',
+        alignItems: 'center',
+        flexGrow: 1,
+        [theme.breakpoints.down('md')]: {
+            alignItems: 'flex-end',
+        },
+    },
+    offersBox: {
+        width: '712px',
+    },
+ 
+}));
 
 function PlaceOffer() {
+
+    const classes = useStyles();
+    const { matchesMobile, matchesTablet} = useMedia();
+
     return (
-        <div id="placeAA" className="placeaa">
-            <Header />
-            <div className="placeaa__wrapper">
-                <div className="placeaa__verify">
-                    <Verify Verify={3} />
-                </div>
-                <form className="placeaa__form">
-                    <div className="placeaa__form__title title">Новое объявление</div>
-                </form>
-            </div>
-            <Footer />
-        </div>
+            <MainLayout title={'Подать объявление'}>
+                {!matchesMobile && !matchesTablet && <Container className={classes.root}>
+                    <Verify Verify={4} />
+                    <Box className={classes.offersBox}>
+                        <Typography variant='h2'>Новое объявление</Typography>
+                        <form>Форма</form>
+                    </Box>
+                </Container>}
+            </MainLayout>
     )
 }
 
