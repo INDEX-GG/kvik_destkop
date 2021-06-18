7ddf1f0e7127253fdd20ce90e12a32ba9227d96f
Aparin.V <stinex.sx@gmail.com>
2021-06-18 19:03:00 +0500

    18.06.2021_v6

--- components/AdCard.js
+++ components/AdCard.js
@@ -30,6 +30,23 @@ function AdCard_component({ objs }) {
         }
     }
 
+
+    const obj = [
+        { id: 1, title: "Toyota Mark II jxz90", objImg: [{ img: "https://source.unsplash.com/random?cars" }, { img: "https://source.unsplash.com/random" }, { img: "https://source.unsplash.com/random?interior" }], oldPrice: 200000, newPrice: 180000, city: "Челябинск", date: "2021-05-19T12:03:51.000000Z", seen: true, status: 0, call: true, message: true, like: true, compare: false, delivery: true, security: true },
+        { id: 2, title: "Toyota Altezza", objImg: [{ img: "https://source.unsplash.com/random?forest" }, { img: "https://source.unsplash.com/random" }, { img: "https://source.unsplash.com/random?interior" }], oldPrice: '', newPrice: 400000, city: "Челябинск", date: "2021-05-19T12:03:51.000000Z", seen: false, status: 1, call: true, message: false, like: true, compare: false, delivery: false, security: true },
+        { id: 3, title: "Lexus RX350", objImg: [{ img: "https://source.unsplash.com/random?land" }, { img: "https://source.unsplash.com/random?interior" }], oldPrice: 800000, newPrice: 790000, city: "Александровск-Сахалинский", date: "2021-05-19T12:03:51.000000Z", seen: false, status: 2, call: true, message: true, like: true, compare: false, delivery: true, security: true },
+        { id: 4, title: "Lexus RX350", objImg: [{ img: "https://source.unsplash.com/random?tools" }], oldPrice: 800000, newPrice: 790000, city: "Александровск-Сахалинский", date: "2021-05-19T12:03:51.000000Z", seen: false, status: 0, call: true, message: true, like: true, compare: false, delivery: true, security: true },
+        { id: 5, title: "Lexus RX350", objImg: [{ img: "https://source.unsplash.com/random" }, { img: "https://source.unsplash.com/random?interior" }, { img: "https://source.unsplash.com/random" }, { img: "https://source.unsplash.com/random" }, { img: "https://source.unsplash.com/random?cars" }, { img: "https://source.unsplash.com/random" }, { img: "https://source.unsplash.com/random?interior" }], oldPrice: 800000, newPrice: 790000, city: "Александровск-Сахалинский", date: "2021-05-19T12:03:51.000000Z", seen: false, status: 1, call: true, message: true, like: true, compare: false, delivery: true, security: true },
+        { id: 6, title: "Lexus RX350", objImg: [{ img: "https://source.unsplash.com/random?moto" }, { img: "https://source.unsplash.com/random" }, { img: "https://source.unsplash.com/random?interior" }], oldPrice: 800000, newPrice: 790000, city: "Александровск-Сахалинский", date: "2021-05-19T12:03:51.000000Z", seen: true, status: 0, call: true, message: true, like: true, compare: false, delivery: true, security: true },
+        { id: 7, title: "Lexus RX350", objImg: [{ img: "https://source.unsplash.com/random?house" }, { img: "https://source.unsplash.com/random" }, { img: "https://source.unsplash.com/random?interior" }], oldPrice: 800000, newPrice: 790000, city: "Александровск-Сахалинский", date: "2021-05-19T12:03:51.000000Z", seen: false, status: 2, call: true, message: true, like: true, compare: false, delivery: true, security: true },
+        { id: 8, title: "Lexus RX350", objImg: [{ img: "https://source.unsplash.com/random?dog" }, { img: "https://source.unsplash.com/random" }, { img: "https://source.unsplash.com/random?interior" }], oldPrice: 800000, newPrice: 790000, city: "Александровск-Сахалинский", date: "2021-05-19T12:03:51.000000Z", seen: true, status: 2, call: true, message: true, like: true, compare: false, delivery: true, security: true },
+        { id: 9, title: "Lexus RX350", objImg: [{ img: "https://source.unsplash.com/random?smail" }, { img: "https://source.unsplash.com/random" }, { img: "https://source.unsplash.com/random?interior" }], oldPrice: 800000, newPrice: 790000, city: "Александровск-Сахалинский", date: "2021-05-19T12:03:51.000000Z", seen: false, status: 1, call: true, message: true, like: true, compare: false, delivery: true, security: true },
+        { id: 10, title: "Lexus RX350", objImg: [{ img: "https://source.unsplash.com/random?animals" }, { img: "https://source.unsplash.com/random" }, { img: "https://source.unsplash.com/random?interior" }], oldPrice: 800000, newPrice: 790000, city: "Александровск-Сахалинский", date: "2021-05-19T12:03:51.000000Z", seen: false, status: 1, call: true, message: true, like: true, compare: false, delivery: true, security: true },
+        { id: 11, title: "Lexus RX350", objImg: [{ img: "https://source.unsplash.com/random?boat" }, { img: "https://source.unsplash.com/random" }, { img: "https://source.unsplash.com/random?interior" }], oldPrice: 800000, newPrice: 790000, city: "Александровск-Сахалинский", date: "2021-05-19T12:03:51.000000Z", seen: true, status: 1, call: true, message: true, like: true, compare: false, delivery: true, security: true },
+        { id: 12, title: "Lexus RX350", objImg: [{ img: "https://source.unsplash.com/random?region" }, { img: "https://source.unsplash.com/random" }, { img: "https://source.unsplash.com/random?interior" }], oldPrice: 800000, newPrice: 790000, city: "Александровск-Сахалинский", date: "2021-05-19T12:03:51.000000Z", seen: false, status: 1, call: true, message: true, like: true, compare: false, delivery: true, security: true }
+      ];
+
+
     /* console.log(objs.map(objs => (objs.title))) */
     const call = true;
     const like = true;
@@ -50,7 +67,7 @@ function AdCard_component({ objs }) {
                                 }}
                                 slidesPerView={1}
                             >
-                                {/* {objs.objsImg.map((img, i) => { return <SwiperSlide key={i}> <img src={img.img} alt="" /></SwiperSlide> })} */}
+                                {/*  {obj.objImg.map((img, i) => { return <SwiperSlide key={i}> <img src={img.img} alt="" /></SwiperSlide> })}  */}
                             </Swiper>
                         </div>
                         <div className="card__top_info">
@@ -68,7 +85,7 @@ function AdCard_component({ objs }) {
                         <div className="card__bottom_info">
                             <div className="card__bottom_info_left">
                                 <span className="old__price">{ /* {ToRubles(objs.oldPrice)}  */}</span>
-                                <span className="new__price">{ ToRubles (objs.price)}</span>
+                                <span className="new__price">{ ToRubles(objs.price)}</span>
                             </div>
                             <div className="card__bottom_info_right">
                                 {objs.delivery ? <span className={!objs.commercial == 0 ? "card_delivery card_delivery-green" : "card_delivery"}></span> : ''}
@@ -77,8 +94,8 @@ function AdCard_component({ objs }) {
                         </div>
                         <div className="card__bottom_info_middle">{objs.title}</div>
                         <div className="card__bottom_info_footer">
-                            <div className="card__bottom_info_footer_left">{objs.commercial === 2 ? objs.address :  ellipsis (objs.address, 15)}</div>
-                            <div className="card__bottom_info_footer_right">{ ToRusDate (objs.created_at)}</div>
+                            <div className="card__bottom_info_footer_left">{objs.commercial === 2 ? objs.address :  ellipsis(objs.address, 15)}</div>
+                            <div className="card__bottom_info_footer_right">{ ToRusDate(objs.created_at)}</div>
                         </div>
                     </div>
                 </div>
@@ -86,5 +103,7 @@ function AdCard_component({ objs }) {
         </Link>
     )))
 }
+
+
 
 export default AdCard_component;
