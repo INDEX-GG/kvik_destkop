e8dd21855dfa9bf5008598cad281acb1d766a566
Yaroslav <slideryo@gmail.com>
2021-06-19 21:43:05 +0500

    some fix in backend

--- pages/api/checkUser.js
+++ pages/api/checkUser.js
@@ -1,57 +1,44 @@
 import { PrismaClient } from '@prisma/client';
-import { truncate } from 'fs/promises';
-import { resolveHref } from 'next/dist/next-server/lib/router/router';
 
-
-export default function handler(req, res){
-
-if (req.method === 'POST'){
-    
-    const prisma = new PrismaClient();
-
-    async function main() 
-    {
-        check();
-//Проверяем на наличие в таблице
-
-        async function check()
-        {
-
-            // Передлать для определния того,что не правильно, логин или пароль 
-            const result = await prisma.users.findUnique({
-
-                where: {
-                    loginUS: {
-                        phone: req.body.phone,
-                        password: req.body.password
+export default function handler(req, res) {
+    if (req.method === 'POST') {
+        const prisma = new PrismaClient();
+        async function main() {
+            check();
+            //Проверяем на наличие в таблице
+            async function check() {
+                // Передлать для определния того,что не правильно, логин или пароль 
+                const user = await prisma.users.findUnique({
+                    where: {
+                        login: {
+                            phone: req.body.phone,
+                            password: req.body.password
+                        }
                     }
+                })
+
+                console.log(user);
+                if (user) {
+                    return res.json({ idUser: user.id });
+                } else {
+                    return res.json({ isset: false });
                 }
-              })
-            
-              console.log(result);
-            if (result) {
-                return res.json({idUser: result.id});
-            } else {
-                return res.json({ isset: false});
             }
+
+
         }
 
+        main()
+            .catch((e) => {
+                console.log("error: " + e);
+                throw e
+            })
+            .finally(async () => {
+                await prisma.$disconnect()
+            })
 
     }
-
-    main()
-    .catch((e) => {
-        console.log("error: " +e);
-        throw e
-    })
-    .finally(async () => {
-        await prisma.$disconnect()
-    })
-
-
-} 
-else {  
-    return res.status(405).json({message: 'method not allowed'})
-}
-
+    else {
+        return res.status(405).json({ message: 'method not allowed' })
+    }
 }
