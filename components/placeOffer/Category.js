255698ec0474b526f50d1a622a765b75793bb431
Yaroslav <slideryo@gmail.com>
2021-06-23 07:58:53 +0500

    23.06.2021

--- components/placeOffer/Category.js
+++ components/placeOffer/Category.js
@@ -1,92 +1,92 @@
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
 import { Box, makeStyles, MenuItem, TextField, Typography } from '@material-ui/core';
 import { useCategory } from '../../hooks/useCategory';
 
 const useStyles = makeStyles((theme) => ({
-   formElem: {
-      display: 'flex',
-      flexDirection: 'row',
-      marginBottom: theme.spacing(3),
-   },
-   formTitleField: {
-      flexGrow: 1,
-      padding: '4px 0',
-   },
-   formInputField: {
-      width: '490px',
-      display: 'flex',
-      flexDirection: 'column',
-      '&>*': {
-          marginBottom: theme.spacing(2),
-      }
-   },
-   input: {
-       width: '264px',
-   },
-   
+    formElem: {
+        display: 'flex',
+        flexDirection: 'row',
+        marginBottom: theme.spacing(3),
+    },
+    formTitleField: {
+        flexGrow: 1,
+        padding: '4px 0',
+    },
+    formInputField: {
+        width: '490px',
+        display: 'flex',
+        flexDirection: 'column',
+        '&>*': {
+            marginBottom: theme.spacing(2),
+        }
+    },
+    input: {
+        width: '264px',
+    },
+
 }));
 
 const Category = () => {
 
     const classes = useStyles();
     const methods = useFormContext();
-    const {categoryMain, categoriesById} = useCategory();
+    const { categoryMain, categoriesById } = useCategory();
 
     console.log(methods.watch('category_1'))
     console.log(methods.watch('category_2'))
     return (
-    <Box className={classes.formElem}>
-         <Typography className={classes.formTitleField}>Категория</Typography>
-         <Box className={classes.formInputField}>
-            <Controller
-               name="category_1"
-               control={methods.control}
-               defaultValue=''
-               render={({ field: { onChange, value }, fieldState: { error } }) => (
-                    <TextField
-                        select
-                        className={classes.input}
-                        variant='outlined'
-                        value={value}
-                        onChange={onChange}
-                        error={!!error} 
-                        helperText={error ? error.message : ' '}>
+        <Box className={classes.formElem}>
+            <Typography className={classes.formTitleField}>Категория</Typography>
+            <Box className={classes.formInputField}>
+                <Controller
+                    name="category_1"
+                    control={methods.control}
+                    defaultValue=''
+                    render={({ field: { onChange, value }, fieldState: { error } }) => (
+                        <TextField
+                            select
+                            className={classes.input}
+                            variant='outlined'
+                            value={value}
+                            onChange={onChange}
+                            onClick={() => methods.setValue('category_2', '')}
+                            error={!!error}
+                            helperText={error ? error.message : ' '}>
                             {categoryMain.map((option, i) => (
                                 <MenuItem key={i} value={option.value}>
                                     {option.label}
                                 </MenuItem>
-                        ))}
-                    </TextField>
-               )}
-               rules={{ required: 'Выбирите Категорию' }}
-            />
+                            ))}
+                        </TextField>
+                    )}
+                    rules={{ required: 'Выбирите Категорию' }}
+                />
 
-            {methods.watch('category_1') && <Controller
-               name="category_2"
-               control={methods.control}
-               defaultValue=''
-               render={({ field: { onChange, value }, fieldState: { error } }) => (
-                    <TextField
-                        select
-                        className={classes.input}
-                        variant='outlined'
-                        value={value}
-                        onChange={onChange}
-                        error={!!error} 
-                        helperText={error ? error.message : ' '}>
+                {methods.watch('category_1') && <Controller
+                    name="category_2"
+                    control={methods.control}
+                    defaultValue=''
+                    render={({ field: { onChange, value }, fieldState: { error } }) => (
+                        <TextField
+                            select
+                            className={classes.input}
+                            variant='outlined'
+                            value={value}
+                            onChange={onChange}
+                            error={!!error}
+                            helperText={error ? error.message : ' '}>
                             {categoriesById(methods.watch('category_1')).map((option, i) => (
                                 <MenuItem key={i} value={option.value}>
                                     {option.label}
                                 </MenuItem>
-                        ))}
-                    </TextField>
-               )}
-               rules={{ required: 'Выбирите Категорию' }}
-            />}
-
-         </Box>
-    </Box>
+                            ))}
+                        </TextField>
+                    )}
+                    rules={{ required: 'Выбирите Категорию' }}
+                />}
+            </Box>
+        </Box>
     )
 }
 
--- hooks/useCategory.js
+++ hooks/useCategory.js
@@ -3,45 +3,45 @@ import navObj from '../components/json/navobj.json';
 export const useCategory = () => {
     console.log(navObj)
 
-    const categoryFirstLevelwithId = (object) => {
+    const categoryIdValueSorted = (object) => {
         let c1 = [],
             c2 = [];
         for (let key = 0; key < Object.keys(object).length; key++) {
             c1 = [...c1, object[Object.keys(object)[key]]]
         };
         c1.forEach(category => {
-            c2 =[...c2, {value: category.id, label: category.name}]
+            c2 = [...c2, { value: category.id, label: category.name }]
         })
-        return (c2.sort((a, b) => a.value - b.value ));
+        return (c2.sort((a, b) => a.value - b.value));
     }
 
     const categoriesById = (id) => {
         let c1 = [],
-        c2 = [],
-        c3 = [];
+            c2 = [],
+            c3 = [];
 
         function obj2Arr(object) {
-            let array =[];
+            let array = [];
             for (let key = 0; key < Object.keys(object).length; key++) {
-                 array = [...array, object[Object.keys(object)[key]]]
+                array = [...array, object[Object.keys(object)[key]]]
             };
             return array
         }
 
         console.log(obj2Arr(navObj))
-        
-    c2 = obj2Arr(navObj).filter(category => category.id == id);
-    console.log('отфильтрованный массив', c2);
-        if ( c2[0] !== undefined ) {
-            console.log(categoryFirstLevelwithId(c2[0].subCategories))
+
+        c2 = obj2Arr(navObj).filter(category => category.id == id);
+        console.log('отфильтрованный массив', c2);
+        if (c2[0] !== undefined) {
+            console.log(categoryIdValueSorted(c2[0].subCategories))
         } else {
             console.log('ошибка катеории второго уровня')
         }
-        return categoryFirstLevelwithId(c2[0].subCategories);
+        return categoryIdValueSorted(c2[0].subCategories);
     }
     console.log(categoriesById(61))
 
-    const categoryMain = (categoryFirstLevelwithId(navObj));
+    const categoryMain = (categoryIdValueSorted(navObj));
 
     return { categoryMain, categoriesById }
 }
