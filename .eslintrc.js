c6bf63dbd8a3b6bff6797486d0007806827d5e5c
Aparin.V <stinex.sx@gmail.com>
2021-07-02 17:55:49 +0500

    02.07.2021

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* module.exports = {
 	'env': {
 		'browser': true,
@@ -36,3 +37,43 @@
 		]
 	}
 }; */
+=======
+module.exports = {
+	// 'env': {
+	// 	'browser': true,
+	// 	'es2021': true
+	// },
+	// 'extends': [
+	// 	'eslint:recommended',
+	// 	'plugin:react/recommended'
+	// ],
+	// 'parserOptions': {
+	// 	'ecmaFeatures': {
+	// 		'jsx': true
+	// 	},
+	// 	'ecmaVersion': 12,
+	// 	'sourceType': 'module'
+	// },
+	// 'plugins': [
+	// 	'react'
+	// ],
+	// 'rules': {
+	// 	'indent': [
+	// 		'error',
+	// 		'tab'
+	// 	],
+	// 	'linebreak-style': [
+	// 		'error',
+	// 		'windows'
+	// 	],
+	// 	'quotes': [
+	// 		'error',
+	// 		'single'
+	// 	],
+	// 	'semi': [
+	// 		'error',
+	// 		'always'
+	// 	]
+	// }
+};
+>>>>>>> 13e5c76cc511cd0a6fb0f3a9dfbd812573be7dc3
---
+++ hooks/useProduct.js
@@ -0,0 +1,56 @@
+import Router from 'next/router';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
+export function useProduct({ router }) {
+    const [productInfo, setProductInfo] = useState({});
+    const [isLoading, setLoading] = useState(true);
+    useEffect(() => {
+        axios.post('/api/getPost', { product_id: router.query.id })
+            .then((result) => {
+                result.data.result.map((items) => {
+                    setProductInfo({
+                        address: items.address,
+                        category_id: items.category_id,
+                        commercial: items.commercial,
+                        created_at: items.created_at,
+                        delivery: items.delivery,
+                        description: items.description,
+                        email: items.email,
+                        id: items.id,
+                        phone: items.phone,
+                        photo: items.photo,
+                        rating: items.rating,
+                        reviewed: items.reviewed,
+                        secure_transaction: items.secure_transaction,
+                        title: items.title,
+                        trade: items.trade,
+                        user_id: items.user_id,
+                        verify_moderator: items.verify_moderator
+                    })
+                })
+                setLoading(false)
+            })
+    }, [router])
+    return {
+        address: productInfo.address,
+        category_id: productInfo.category_id,
+        commercial: productInfo.commercial,
+        created_at: productInfo.created_at,
+        delivery: productInfo.delivery,
+        description: productInfo.description,
+        email: productInfo.email,
+        id: productInfo.id,
+        phone: productInfo.phone,
+        photo: productInfo.photo,
+        rating: productInfo.rating,
+        reviewed: productInfo.reviewed,
+        secure_transaction: productInfo.secure_transaction,
+        title: productInfo.title,
+        trade: productInfo.trade,
+        user_id: productInfo.user_id,
+        verify_moderator: productInfo.verify_moderator,
+        isLoading,
+    }
+}
+
