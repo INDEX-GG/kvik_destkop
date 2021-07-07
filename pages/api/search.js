4d9859162fc14c81463047ffe4c54ba40bbebe9b
Aparin.V <stinex.sx@gmail.com>
2021-07-07 13:46:01 +0500

    07.07.2021

--- pages/api/search.js
+++ pages/api/search.js
@@ -12,11 +12,12 @@ export default function handler(req, res) {
 
             async function getPost(ids) {
 
-                const results = await prisma.$queryRaw(`WITH ser AS (SELECT id,category_id,price,photo,rating,created_at,delivery,reviewed,address,phone,trade,verify_moderator,commercial,secure_transaction,title,email,title,description FROM "public"."posts" WHERE title ~ ${ids} ) SELECT categories.name,categories.alias,posts.user_id,posts.secure_transaction,posts.description,posts.id,posts.category_id,posts.price,posts.photo,posts.rating,posts.created_at,posts.delivery,posts.reviewed,posts.address,posts.phone,posts.trade,posts.verify_moderator,posts.title,posts.email FROM "ser","categories","posts" WHERE ser.category_id = categories.id AND ser.id = posts.id`)
+                const results = await prisma.$queryRaw(`WITH ser AS (SELECT id,category_id,price,photo,rating,created_at,delivery,reviewed,address,phone,trade,verify_moderator,commercial,secure_transaction,title,email,title,description FROM "public"."posts" WHERE (title ~* '${ids}') OR (description ~* '${ids}') ) SELECT categories.name,categories.alias,posts.user_id,posts.secure_transaction,posts.description,posts.id,posts.category_id,posts.price,posts.photo,posts.rating,posts.created_at,posts.delivery,posts.reviewed,posts.address,posts.phone,posts.trade,posts.verify_moderator,posts.title,posts.email FROM "ser","categories","posts" WHERE ser.category_id = categories.id AND ser.id = posts.id`)
                 return results;
             }
-
-            const results = await getPost(+req.body.product_id);
+            console.log("data",req.body)
+            const results = await getPost(req.body.product_name);
+            console.log(results)
             res.json({ result: results });
 
         }
