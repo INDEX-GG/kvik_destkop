dd4a997005896c112d309faf0ab7fe10c491e114
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-07-12 13:52:44 +0500

    12.07 citySearch_api 2.0

--- pages/api/citySearch.js
+++ pages/api/citySearch.js
@@ -5,8 +5,9 @@ export default function handler(req, res) {
         async function main() {
             let {name, parent_id} = req.body
             const lowername = name.toLowerCase()
-                // Добавить обработку регистров
-                const cities = await prisma.$queryRaw(`SELECT name, parent_id FROM cities WHERE LOWER (name) LIKE '${lowername}%'`)     // LIMIT 5
+                const cities = await prisma.$queryRaw(`SELECT name, parent_id FROM cities WHERE LOWER (name) LIKE '${lowername}%'`)     // LIMIT?
+                const citiesMiddle = await prisma.$queryRaw(`SELECT name, parent_id FROM cities WHERE LOWER (name) LIKE '%${lowername}%' AND LOWER (name) NOT LIKE '${lowername}%'`)
+                console.log(citiesMiddle)
                 const RES = []
                     for (let value of cities) {
                         if (value.parent_id == "0") {
@@ -30,10 +31,30 @@ export default function handler(req, res) {
                                 RES.push([{ object : value, parent : results1, grandparent : results2 }])
                             }
                         }
-
                     }
-                    console.log(RES)
-                    //res.json(cities)
+                    for (let value of citiesMiddle) {
+                        if (value.parent_id == "0") {
+                            RES.push([{ object : value }])
+                        } else {
+                            const parentNum = parseInt(value.parent_id)
+                            const results1 = await prisma.cities.findFirst({
+                                where: {
+                                    id: parentNum
+                                }
+                            })
+                            if (((results1).parent_id) == 0 || ((results1).parent_id) == null) {
+                                RES.push([{ object : value, grandparent : results1 }])
+                            } else {
+                                const parentNum2 = parseInt(results1.parent_id)
+                                const results2 = await prisma.cities.findFirst({
+                                    where: {
+                                        id: parentNum2
+                                    }
+                                })
+                                RES.push([{ object : value, parent : results1, grandparent : results2 }])
+                            }
+                        }
+                    }
                     res.json(RES)
         }
         main()
