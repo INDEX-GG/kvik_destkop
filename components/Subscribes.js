a45ae34fbc96ab875b2fddea7e53e9c7babe3253
Rodion <rodionpyatigroskiy@gamil.com>
2021-07-12 16:50:25 +0500

    modal account

--- components/Subscribes.js
+++ components/Subscribes.js
@@ -1,23 +1,23 @@
-import StarRating from './StarRating';
+import StarRating from "./StarRating";
 
-function Subscribes(data) {
-    return (
-        <div className="subscribers_subscriptions_main">
-            <div className="subscribers_subscriptions">
-                <div className="subscribers_subscriptions__user_info">
-                    <div className="subscribers_subscriptions__avatar">
-                        <img src="https://source.unsplash.com/random?interior" alt="avatar" />
-                    </div>
-                    <div className="subscribers_subscriptions__user_name">
-                        Имя Пользователя
-                    </div>
-                </div>
-                <div className="subscribers_subscriptions__user_rating">
-                    <StarRating rating={data.data.userRate} />
-                </div>
-            </div>
+function Subscribes({ data }) {
+  console.log(data);
+  return (
+    <div className="subscribers_subscriptions_main">
+      <div className="subscribers_subscriptions">
+        <div className="subscribers_subscriptions__user_info">
+          <div className="subscribers_subscriptions__avatar">
+            <img src="https://source.unsplash.com/random?interior" alt="avatar" />
+          </div>
+          <div className="subscribers_subscriptions__user_name">Имя Пользователя</div>
         </div>
-    )
+        <div className="subscribers_subscriptions__user_rating">
+          <div className="subscribers_subscriptions__user_number-raiting">2.0</div>
+          <StarRating rating={data} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Subscribes;
