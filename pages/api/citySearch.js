78e27da5d7f9b135586ad568c7ad04e33e8b51c4
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-07-12 11:24:32 +0500

    12.07 citySearch_api

--- pages/api/citySearch.js
+++ pages/api/citySearch.js
@@ -1,54 +1,41 @@
 import { PrismaClient } from '@prisma/client';
 export default function handler(req, res) {
-
     if (req.method === 'POST') {
         const prisma = new PrismaClient();
         async function main() {
             let {name, parent_id} = req.body
-            if (parent_id == "0") {
-                res.status(200).json({message: 'this is country'})
-            } else {
-
+            const lowername = name.toLowerCase()
                 // Добавить обработку регистров
-                const cities = await prisma.$queryRaw(`SELECT name, parent_id FROM cities WHERE name LIKE '${name}%'`)     // LIMIT 5
+                const cities = await prisma.$queryRaw(`SELECT name, parent_id FROM cities WHERE LOWER (name) LIKE '${lowername}%'`)     // LIMIT 5
+                const RES = []
+                    for (let value of cities) {
+                        if (value.parent_id == "0") {
+                            RES.push([{ object : value }])
+                        } else {
+                            const parentNum = parseInt(value.parent_id)
+                            const results1 = await prisma.cities.findFirst({
+                                where: {
+                                    id: parentNum
+                                }
+                            })
+                            if (((results1).parent_id) == 0 || ((results1).parent_id) == null) {
+                                RES.push([{ object : value, grandparent : results1 }])
+                            } else {
+                                const parentNum2 = parseInt(results1.parent_id)
+                                const results2 = await prisma.cities.findFirst({
+                                    where: {
+                                        id: parentNum2
+                                    }
+                                })
+                                RES.push([{ object : value, parent : results1, grandparent : results2 }])
+                            }
+                        }
 
-                // const RES = []
-                //
-                // for (let value of cities) {
-                //
-                //
-                //
-                //     const parentNum = parseInt(value.parent_id)
-                //     const results1 = await prisma.cities.findFirst({
-                //         where: {
-                //             id: parentNum
-                //         }
-                //     })
-                //     if (((results1).parent_id) == 0) {
-                //         res.json({country : results1})
-                //     } else {
-                //         const parentNum2 = parseInt(results1.parent_id)
-                //         const results2 = await prisma.cities.findFirst({
-                //             where: {
-                //                 id: parentNum2
-                //             }
-                //         })
-                //
-                //         RES.push([{country : results2, region : results1, city : value }])
-                //         //res.json({country : results2, region : results1, city : value })
-                //     }
-                //
-                //
-                //
-                //
-                //
-                // }
-                //     console.log(RES)
-                    res.json(cities)
-
-            }
+                    }
+                    console.log(RES)
+                    //res.json(cities)
+                    res.json(RES)
         }
-
         main()
             .catch((e) => {
                 console.log("error: " + e);
