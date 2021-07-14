db767ca4140a9d5442415dce0f3faa30b89f522b
asas <asas>
2021-07-14 12:58:36 +0500

    add search in category api 1.1

--- pages/api/searchInsideCategory.js
+++ pages/api/searchInsideCategory.js
@@ -9,17 +9,20 @@ export default function handler(req, res) {
             const text = req.body.text
             const lowerText = text.toLowerCase()
 
+            // Поиск по всем категориям по совпадениям title, description
             if (categoryId == undefined || categoryId == '') {
-                const results = await prisma.$queryRaw(`SELECT * FROM posts WHERE LOWER (title) LIKE '%${lowerText}%'`)
+                const results = await prisma.$queryRaw(`SELECT id, category_id, price, photo, rating, created_at, delivery, reviewed, address, phone, trade, verify_moderator, commercial, secure_transaction, title, email FROM posts WHERE LOWER (title) LIKE '%${lowerText}%' OR LOWER (description) LIKE '%${lowerText}%'`)
                 res.json({results: results})
 
+
+            // Поиск по категории из тела по совпадениям title, description
             } else {
-                const results = await prisma.$queryRaw(`SELECT * FROM posts WHERE category_id = ${categoryIdInt} AND LOWER (title) LIKE '%${lowerText}%'`)
+                const results = await prisma.$queryRaw(`SELECT id, category_id, price, photo, rating, created_at, delivery, reviewed, address, phone, trade, verify_moderator, commercial, secure_transaction, title, email FROM posts WHERE category_id = ${categoryIdInt} AND LOWER (title) LIKE '%${lowerText}%' OR category_id = ${categoryIdInt} AND LOWER (description) LIKE '%${lowerText}%'`)
                 res.json({ results : results });
             }
 
-
         }
+
 
         main()
             .catch((e) => {
