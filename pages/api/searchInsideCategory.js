f5a00b2d7afb7d8663a2b9bdfadaf2786f071684
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-07-14 14:19:26 +0500

    14.07.21

--- pages/api/searchInsideCategory.js
+++ pages/api/searchInsideCategory.js
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 export default function handler(req, res) {
 
+
     if (req.method === 'POST') {
         const prisma = new PrismaClient();
         async function main() {
