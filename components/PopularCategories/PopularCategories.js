fd83f6edd739263a8a842ca4fd15d83ec7b9a616
Vladislav <stinex.sx@gmail.com>
2021-07-28 17:13:47 +0500

    28.07.2021

--- components/PopularCategories/PopularCategories.js
+++ components/PopularCategories/PopularCategories.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Box, makeStyles, Typography } from "@material-ui/core";
+import { Link, makeStyles, Typography } from "@material-ui/core";
 import { Icons } from '../../UI/icons/popularCategories/pc_icons';
 import PopularIcon from './PopularIcon';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { useRouter } from "next/router"
 
 const useStyles = makeStyles((theme) => ({
 	slider: {
@@ -13,25 +14,28 @@ const useStyles = makeStyles((theme) => ({
 	}
 }));
 
+console.log(Icons)
+
 const PopularCategories = () => {
+	const router = useRouter()
 	const classes = useStyles();
 	return (
 		<>
 			<Typography variant='h2'>Популярные категории</Typography>
-				<Swiper
-					className={classes.slider}
-					spaceBetween={0}
-					slidesPerView={'auto'}
-					loop={true}
-				>
-					{Icons.map((icon, i) => {
-						return (
-							<SwiperSlide key={i} className={classes.slide}>
-								<PopularIcon icon={icon} random={Math.round(Math.random() * 360)} />
-							</SwiperSlide>
-						)
-					})}
-				</Swiper>
+			<Swiper
+				className={classes.slider}
+				spaceBetween={0}
+				slidesPerView={'auto'}
+				loop={true}
+			>
+				{Icons.map((icon, i) => {
+					return (
+						<SwiperSlide key={i} className={classes.slide} >
+							<Link onClick={() => router.push(`/search/${icon.alias}`)}>	<PopularIcon icon={icon} random={Math.round(Math.random() * 360)} /></Link>
+						</SwiperSlide>
+					)
+				})}
+			</Swiper>
 		</>
 	)
 }
