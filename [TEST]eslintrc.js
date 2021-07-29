fbbf287d2548444a874941405b19b7fa3c9b95a7
Yaroslav <slideryo@gmail.com>
2021-07-29 20:15:48 +0500

    2907

---
+++ [TEST]eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true,
+		'node': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'next', 
+		'next/core-web-vitals'
+	],
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 12,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		]
+	}
+};
