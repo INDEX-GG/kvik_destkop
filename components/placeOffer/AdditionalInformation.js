ad68539c012cc2f36ac5ec3b490659847c89b764
Vladislav <stinex.sx@gmail.com>
2021-08-11 17:56:11 +0500

    11.08.2021

--- components/placeOffer/AdditionalInformation.js
+++ components/placeOffer/AdditionalInformation.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
 import { Box, makeStyles, TextField, Typography, MenuItem } from '@material-ui/core';
 
@@ -21,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const AdditionalInformation = (data) => {
-    console.log(data)
+    console.log('=======>', data.newOBJ)
 
     const classes = useStyles();
     const methods = useFormContext();
@@ -29,69 +30,125 @@ const AdditionalInformation = (data) => {
     return (
         <Box className={classes.formElem}>
             {Object.entries(data.newOBJ).map((item, key) => (
+                <>
+                    {item[1].fields.marks !== undefined && (
+                        <>
+                            <Box key={key} className={classes.formInputMainField}>
+                                <Typography className={classes.formTitleField}>Марка</Typography>
+                                <Box className={classes.formInputField}>
+                                    <Controller
+                                        name={"modelsAuto"}
+                                        control={methods.control}
+                                        render={({ field: { onChange, value }, fieldState: { error } }) => (
+                                            <TextField
+                                                select
+                                                className={classes.input}
+                                                variant='outlined'
+                                                value={value}
+                                                onChange={onChange}
+                                                error={!!error}
+                                                helperText={error ? error.message : ' '}>
+                                                {item[1].fields.marks.map((item, i) => (
+                                                    <MenuItem key={i} value={item.name}>
+                                                        {item.name}
+                                                    </MenuItem>
+                                                ))}
+                                            </TextField>
+                                        )}
+                                        rules={{ required: 'Выбирите ' }}
+                                    />
+                                </Box>
+                            </Box>
 
-                <Box key={key} className={classes.formInputMainField}>
-                    {item[1].fields === 'NULL' ?
-                        <>
-                            <Typography className={classes.formTitleField}>{item[1].name}</Typography>
-                            <Box className={classes.formInputField}>
-                                <Controller
-                                    name={item[1].alias}
-                                    control={methods.control}
-                                    defaultValue=''
-                                    render={({ field: { onChange, value }, fieldState: { error } }) => (
-                                        <TextField
-                                            variant='outlined'
-                                            type="text"
-                                            fullWidth
-                                            autoComplete="on"
-                                            value={value}
-                                            onChange={onChange}
-                                            error={!!error} helperText={error ? error.message : ' '} />
-                                    )}
-                                    rules={{ required: 'Укажите ваше местоположение...' }}
-                                />
+                            <Box key={key} className={classes.formInputMainField}>
+                                {methods.watch('modelsAuto') &&
+                                    <>
+                                        <Typography className={classes.formTitleField}>Модель</Typography>
+                                        <Box className={classes.formInputField}>
+                                            <Controller
+                                                name="carModel"
+                                                control={methods.control}
+                                                render={({ field: { onChange, value }, fieldState: { error } }) => (
+                                                    <TextField
+                                                        select
+                                                        className={classes.input}
+                                                        variant='outlined'
+                                                        value={value}
+                                                        onChange={onChange}
+                                                        error={!!error}
+                                                        helperText={error ? error.message : ' '}
+                                                    >
+                                                        {item[1].fields.marks.filter(item => item.name === methods.watch('modelsAuto')).map((item, i) => item.models.map((item, i) =>
+                                                            <MenuItem key={i} value={item.name}>
+                                                                {item.name}
+                                                            </MenuItem>
+                                                        ))
+                                                        }
+                                                    </TextField>
+                                                )}
+                                                rules={{ required: 'Выбирите ' }}
+                                            />
+                                        </Box>
+                                    </>
+                                }
                             </Box>
                         </>
-                        :
-                        <>
-                            <Typography className={classes.formTitleField}>{item[1].name}</Typography>
-                            <Box className={classes.formInputField}>
-                                <Controller
-                                    name="alias1"
-                                    control={methods.control}
-                                    defaultValue=''
-                                    render={({ field: { onChange, value }, fieldState: { error } }) => (
-                                        <TextField
-                                            select
-                                            className={classes.input}
-                                            variant='outlined'
-                                            value={value}
-                                            onChange={onChange}
-                                            // onClick={() => {
-                                            //     methods.setValue('alias2', '');
-                                            //     methods.setValue('alias3', '');
-                                            //     methods.setValue('alias4', '');
-                                            //     methods.unregister('alias2', '');
-                                            //     methods.unregister('alias3', '');
-                                            //     methods.unregister('alias4', '');
-                                            // }}
-                                            // error={!!error}
-                                            helperText={error ? error.message : ' '}>
-                                            {item[1].fields.map((option, i) => (
-                                                <MenuItem key={i} value={option}>
-                                                    {option}
-                                                </MenuItem>
-                                            ))}
-                                        </TextField>
-                                    )}
-                                    rules={{ required: 'Выбирите ' }}
-                                />
-                            </Box>
-                        </>
-                    }
-                </Box>
-            ))}
+                    )}
+                    <Box key={key} className={classes.formInputMainField}>
+
+                        {item[1].fields.marks === undefined &&
+                            (item[1].fields === 'NULL' ?
+                                <>
+                                    <Typography className={classes.formTitleField}>{item[1].name}</Typography>
+                                    <Box className={classes.formInputField}>
+                                        <Controller
+                                            name={item[1].alias}
+                                            control={methods.control}
+                                            defaultValue=''
+                                            render={({ field: { onChange, value }, fieldState: { error } }) => (
+                                                <TextField
+                                                    variant='outlined'
+                                                    type="text"
+                                                    fullWidth
+                                                    autoComplete="on"
+                                                    value={value}
+                                                    onChange={onChange}
+                                                    error={!!error} helperText={error ? error.message : ' '} />
+                                            )}
+                                            rules={{ required: 'Укажите ваше местоположение...' }}
+                                        />
+                                    </Box>
+                                </>
+                                :
+                                <>
+                                    <Typography className={classes.formTitleField}>{item[1].name}</Typography>
+                                    <Box className={classes.formInputField}>
+                                        <Controller
+                                            name={'aliasFields' + key}
+                                            control={methods.control}
+                                            render={({ field: { onChange, value }, fieldState: { error } }) => (
+                                                <TextField
+                                                    select
+                                                    className={classes.input}
+                                                    variant='outlined'
+                                                    value={value}
+                                                    onChange={onChange}
+                                                    error={!!error}
+                                                    helperText={error ? error.message : ' '}>
+                                                    {item[1].fields.map((option, i) => (
+                                                        <MenuItem key={i} value={option}>
+                                                            {option}
+                                                        </MenuItem>
+                                                    ))}
+                                                </TextField>
+                                            )}
+                                            rules={{ required: 'Выбирите ' }}
+                                        />
+                                    </Box>
+                                </>
+                            )}
+                    </Box>
+                </>))}
         </Box>
     )
 }
--- hooks/useCategoryPlaceOffer.js
+++ hooks/useCategoryPlaceOffer.js
@@ -1,32 +1,29 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
-
-
-
-
 export const useCategoryPlaceOffer = (data) => {
-    console.log(data)
+
     let name = data?.toLowerCase();
     const [zxc, setZxc] = useState(undefined)
 
     let newOBJ = [];
-    if (zxc?.length !== undefined) {
+    if (zxc?.length != undefined) {
         for (var i = 0; i < zxc?.length; i++) {
             newOBJ?.push(JSON.parse(((zxc[i]?.split('"').join(''))?.split("'").join('"'))))
         }
     } else {
-        newOBJ = []
+        newOBJ = undefined
     }
-  
+
     useEffect(() => {
-        if (name !== undefined) {
+        if (name !== undefined && name !== 'undefined') {
             axios.get(`/subcategories/` + name + `.json`)
                 .then((result) => setZxc(result.data[name]))
                 .catch((e) => {
                     console.log("error: " + e);
-                    throw e
                 })
+        } else {
+            setZxc()
         }
     }, [name])
     return { ...newOBJ }
