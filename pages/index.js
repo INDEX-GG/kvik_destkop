1208485f5387645e37de3fad2413bb3ea1430e6c
Yaroslav_Makarov <slideryo@gmail.com>
2021-08-17 17:03:53 +0500

    4deploy-wo-ssr

--- pages/index.js
+++ pages/index.js
@@ -41,10 +41,10 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const Index = ({ offers }) => {
+const Index = (/* { offers } */) => {
 
   const { matchesMobile, matchesTablet, matchesLaptop, matchesDesktop, matchesHD } = useMedia();
-  const [data, setData] = useState(offers);
+  const [data, setData] = useState([]);
 
   const classes = useStyles();
 
@@ -71,52 +71,52 @@ const Index = ({ offers }) => {
   )
 }
 
-export async function getStaticProps() {
-  const prisma = new PrismaClient();
-  async function main(of) {
-    //Этот запрос нужно будет связать с таблицей
-    async function getPost() {
-      const results = await prisma.posts.findMany({
-        skip: of,
-        take: 10,
-        select: {
-          id: true,
-          category_id: true,
-          price: true,
-          old_price: true,
-          photo: true,
-          rating: true,
-          created_at: true,
-          delivery: true,
-          reviewed: true,
-          address: true,
-          phone: true,
-          trade: true,
-          verify_moderator: true,
-          commercial: true,
-          secure_transaction: true,
-          title: true,
-          email: true,
-        },
-      });
-      return results;
-    }
-    const results = await getPost();
+// export async function getStaticProps() {
+//   const prisma = new PrismaClient();
+//   async function main(of) {
+//     //Этот запрос нужно будет связать с таблицей
+//     async function getPost() {
+//       const results = await prisma.posts.findMany({
+//         skip: of,
+//         take: 10,
+//         select: {
+//           id: true,
+//           category_id: true,
+//           price: true,
+//           old_price: true,
+//           photo: true,
+//           rating: true,
+//           created_at: true,
+//           delivery: true,
+//           reviewed: true,
+//           address: true,
+//           phone: true,
+//           trade: true,
+//           verify_moderator: true,
+//           commercial: true,
+//           secure_transaction: true,
+//           title: true,
+//           email: true,
+//         },
+//       });
+//       return results;
+//     }
+//     const results = await getPost();
 
-    return results;
-  }
+//     return results;
+//   }
 
-  let res = await main(0)
-    .catch((e) => {
-      console.log("error: " + e);
-      throw e;
-    })
-    .finally(async () => {
-      await prisma.$disconnect();
-    });
+//   let res = await main(0)
+//     .catch((e) => {
+//       console.log("error: " + e);
+//       throw e;
+//     })
+//     .finally(async () => {
+//       await prisma.$disconnect();
+//     });
 
-  const offers = JSON.parse(JSON.stringify(res));
-  return { props: { offers } };
-}
+//   const offers = JSON.parse(JSON.stringify(res));
+//   return { props: { offers } };
+// }
 
 export default Index;
