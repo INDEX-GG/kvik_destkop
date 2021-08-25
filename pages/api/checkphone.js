1c7f699b3f6c505f8b9a813857b0dcf035820215
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-08-25 16:14:48 +0500

    25.08.2021 api update

--- pages/api/checkphone.js
+++ pages/api/checkphone.js
@@ -1,47 +1,53 @@
 import axios from 'axios';
 import qs from 'qs';
 
-export default function handler(req, resolve) {
+export default async function handler(req, res) {
     if (req.method === 'POST') {
-        const urlAuth = 'https://pbx-guru.web.pbxmaker.ru/index.php/restapi/auth',
-              urlCall = 'https://pbx-guru.web.pbxmaker.ru/index.php/restapi/number/call-auth',
-              urlApprove = 'https://pbx-guru.web.pbxmaker.ru/index.php/restapi/number/approve',
-              dataAuth = qs.stringify({ 'grant_type': 'password', 'scope': 'users', 'client_id': '1kvik', 'client_secret': 'bqnqxnhwdb4' }),
-              phoneNumber = qs.stringify({'caller_id': JSON.parse(JSON.stringify(req.body.phone))});
 
-axios.post(urlAuth, dataAuth, {headers: {
-  'content-type': 'application/x-www-form-urlencoded'
-}})
-.then((res)=> {
-    // console.log('Первый then:', res.data)
-    const token = res.data.access_token;
-    axios.post(urlCall, phoneNumber, {headers: {
-      'content-type': 'application/x-www-form-urlencoded', 'Authorization': `Bearer ${token}`
-    }})
-    .then((res) => {
-    // console.log('Второй then:', res.data)
-    
-    const callerId = res.data.caller_id,
-          tmpCallerId = res.data.tmp_caller_id,
-          dataApprove = qs.stringify({
-            'action': 'call-auth',
-            'caller_id': callerId,
-            'tmp_caller_id': tmpCallerId
-          });
-          
-    axios.post(urlApprove, dataApprove, {headers: {
-      'content-type': 'application/x-www-form-urlencoded', Authorization: `Bearer ${token}`
-    }}) 
-    .then(() => {
-      // console.log('Третий then:', res.data)
-      return (resolve.json(tmpCallerId))
-      })
-	  .catch(e => console.error(`ошибка api checkphone - 3${e}`))
-    })
-	.catch(e => console.error(`ошибка api checkphone - 2${e}`))
-  })
-  .catch(e => console.error(`ошибка api checkphone - 1${e}`))
-  } else {
-    return resolve.status(405).json({message: 'method not allowed'})
-  }
+        const main = async () => {
+            const urlAuth = 'https://pbx-guru.web.pbxmaker.ru/index.php/restapi/auth',
+                urlCall = 'https://pbx-guru.web.pbxmaker.ru/index.php/restapi/number/call-auth',
+                urlApprove = 'https://pbx-guru.web.pbxmaker.ru/index.php/restapi/number/approve',
+                dataAuth = qs.stringify({ 'grant_type': 'password', 'scope': 'users', 'client_id': '1kvik', 'client_secret': 'bqnqxnhwdb4' }),
+                phoneNumber = qs.stringify({'caller_id': JSON.parse(JSON.stringify(req.body.phone))});
+
+            const auth = await axios.post(urlAuth, dataAuth, {headers: {
+                    'content-type': 'application/x-www-form-urlencoded'
+                }})
+                .then(r => r.data)
+
+            const call = await axios.post(urlCall, phoneNumber, {headers: {
+                    'content-type': 'application/x-www-form-urlencoded', 'Authorization': `Bearer ${auth.access_token}`
+                }})
+                .then(r => r.data)
+
+            const dataApprove = qs.stringify({
+                'action': 'call-auth',
+                'caller_id': call.caller_id,
+                'tmp_caller_id': call.tmp_caller_id
+            });
+
+            const approve = await axios.post(urlApprove, dataApprove, {headers: {
+                    'content-type': 'application/x-www-form-urlencoded', Authorization: `Bearer ${auth.access_token}`
+                }})
+            approve
+            return await call.tmp_caller_id;
+        }
+
+        try {
+            let response = await main();
+            res.status(200);
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(response))
+        }
+        catch (e) {
+            console.error(`ошибка api checkphone${e}`)
+            res.json('ошибка api checkphone', e)
+            res.status(405).end();
+        }
+
+    } else {
+        res.json({ message: 'method not allowed' })
+        res.status(405).end()
+    }
 }
--- pages/api/subscribers.js
+++ pages/api/subscribers.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
 	if (req.method === 'POST') {
 		const prisma = new PrismaClient();
 
@@ -8,20 +8,6 @@ export default function handler(req, res) {
 			const user_id = req.body.user_id;
 			const subscriber_id = req.body.subscriber_id;
 			const userIdInt = Number(user_id);
-
-			let sub = await prisma.$queryRaw(`SELECT subscribers FROM users WHERE id = ${userIdInt}`)
-			if (sub[0].subscribers == null || sub[0].subscriptions === '') {
-				const obj = {
-					where:
-					{
-						id: userIdInt
-					},
-					data: {
-						subscribers: '[]'
-					}
-				}
-				await prisma.users.update(obj);
-			}
 			const subscribers = await prisma.users.findFirst({
 				where: {
 					id: userIdInt
@@ -31,7 +17,6 @@ export default function handler(req, res) {
 					subscribers: true
 				}
 			});
-
 			let answer = 'none';
 			let preList = subscribers['subscribers'].substring(1);
 			let preList2 = preList.substring(0, preList.length - 1);
@@ -46,12 +31,10 @@ export default function handler(req, res) {
 				list.push(subscriber_id)
 				answer = 'post'
 			}
-
 			var index_1 = list.indexOf('')
 			if (index_1 > -1) {
 				list.splice(index_1, 1)
 			}
-
 			const obj = {
 				where:
 				{
@@ -64,14 +47,24 @@ export default function handler(req, res) {
 			await prisma.users.update(obj);
 			return answer;
 		}
-		main()
-			.then(r => res.json(r))
-			.catch(e => console.error(`ошибка api subscribers${e}`))
-			.finally(async () => {
-				await prisma.$disconnect()
-			})
-	}
-	else {
-		res.status(405).json({ message: 'method not allowed' })
+
+		try {
+			let response = await main();
+			res.status(200);
+			res.setHeader('Content-Type', 'application/json');
+			res.end(JSON.stringify(response))
+		}
+		catch (e) {
+			console.error(`ошибка api subscribers${e}`)
+			res.json('ошибка api subscribers', e)
+			res.status(405).end();
+		}
+		finally {
+			await prisma.$disconnect();
+		}
+
+	} else {
+		res.json({ message: 'method not allowed' })
+		res.status(405).end()
 	}
 }
