c27920f3318fdbeea53ab7c4e8ed4756b1bdd2c6
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-08-27 15:25:05 +0500

    27.08.2021 api update

--- pages/api/test.js
+++ pages/api/test.js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
 
 
         const main = async () => {
-            return {message:'this is prod'}
+            return {message:'this is maindev'}
         }
 
         main()
