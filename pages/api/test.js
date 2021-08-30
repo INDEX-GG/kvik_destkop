7adf9d2ae2b8a6ab8f95d3288222c2672b8106ef
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-08-30 15:51:43 +0500

    test

--- pages/api/test.js
+++ pages/api/test.js
@@ -3,7 +3,7 @@ export default function handler(req, res) {
 
 
         const main = async () => {
-            return {message:'test6'}
+            return {message:'test7'}
         }
 
         main()
