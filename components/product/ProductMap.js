79e5f8b34816850986d88d96d0052df003405c6e
Родион Пятигорский <pyatigorskyi@kvikdomain.local>
2021-09-22 11:06:24 +0500

    change

--- components/product/ProductMap.js
+++ components/product/ProductMap.js
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
-import ProductYMAP from './ProductYMAP';
+// import ProductYMAP from './ProductYMAP';
 
 
-const ProductMap = ({address, coordinates, mobile}) => {
+const ProductMap = ({address, mobile}) => {
 
 
-	const [collMap, setCollMap] = useState(true);
-	const handleCollMap = e => {
-		e.preventDefault();
-		if (collMap) {
-			setCollMap(false);
-		} else {
-			setCollMap(true);
-		}
-	}
+	const [collMap, /** setCollMap */] = useState(true);
+	// const handleCollMap = e => {
+	// 	e.preventDefault();
+	// 	if (collMap) {
+	// 		setCollMap(false);
+	// 	} else {
+	// 		setCollMap(true);
+	// 	}
+	// }
 
 	return (
 		<div className="productPageCharacterMapBlock" style={collMap ? { paddingBottom: 0 } : { paddingBottom: '18px' }} >
@@ -22,15 +22,15 @@ const ProductMap = ({address, coordinates, mobile}) => {
 					<div className="productPageCharacterLocality">
 						{mobile && <div className="productLocality">Местоположение</div>}
 						<div>{address == undefined ? '' : address.length > 45 ? address.slice(0, 45) + '...' : address}</div>
-						<a className={`productPageCharacterMapSwitch highlight underline ${collMap ? ('') : ('collMapSw')}`} onClick={e => handleCollMap(e)}>На карте</a>
-						<span></span>
+						{/* <a className={`productPageCharacterMapSwitch highlight underline ${collMap ? ('') : ('collMapSw')}`} onClick={e => handleCollMap(e)}>На карте</a>
+						<span></span> */}
 					</div>
 
 				}
 				<div className="productPageCharacterMap" style={collMap ? { height: 0 } : { height: '400px' }}>
 					{/* проблема с CORS */}
 					{/* <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ade278bb067489a15a031480c20e3318914d391acd3e1995348d759fa5baa2167&amp;source=constructor" width="618" height="400" frameBorder="0"></iframe> */}
-					<ProductYMAP coordinates={coordinates}/>
+					{/* <ProductYMAP coordinates={coordinates}/> */}
 				</div>
 			</div>
 	)
