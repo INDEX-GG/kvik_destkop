92f279db7e1ced6e901ed616a96238685e999d6e
Родион Пятигорский <pyatigorskyi@kvikdomain.local>
2021-09-28 13:06:28 +0500

    end

---
+++ components/DadataSuggest.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react'
+import { AddressSuggestions } from 'react-dadata';
+import 'react-dadata/dist/react-dadata.css';
+import ProductYMAP from './product/ProductYMAP';
+
+const DadataSuggest = () => {
+
+	const [value, setValue] = useState();
+	
+	// const handleSumbit = (e) => {
+	// 	var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
+	// 	var token = "3fa959dcd662d65fdc2ef38f43c2b699a3485222";
+	// 	var query = value
+
+	// 	console.log(value)
+
+	// 	var options = {
+	// 	method: "POST",
+	// 	mode: "cors",
+	// 	headers: {
+	// 		"Content-Type": "application/json",
+	// 		"Accept": "application/json",
+	// 		"Authorization": "Token " + token
+	// 	},
+	// 	body: JSON.stringify({query: query})
+	// 	}
+	// 	fetch(url, options)
+	// 	.then(response => response.text())
+	// 	.then(result => console.log(JSON.parse(result)))
+	// 	.catch(error => console.log("error", error));
+	// }
+
+
+	console.log(value)
+
+	return (
+		<div>
+			{/* <form 
+			onSubmit={handleSumbit} 
+			onKeyDown={e => {
+				e.key == "Enter" ? handleSumbit(e) : null
+			}}>
+				<AddressSuggestions token="3fa959dcd662d65fdc2ef38f43c2b699a3485222" value={value} onChange={setValue} />;
+			</form> */}
+			<AddressSuggestions token="3fa959dcd662d65fdc2ef38f43c2b699a3485222" value={value} onChange={setValue} />;
+			<ProductYMAP coordinates={value ? [value.data.geo_lat, value.data.geo_lon] : [55.7558, 37.6173]}/>
+		</div>
+	)
+}
+
+export default DadataSuggest
--- pages/maptest.js
+++ pages/maptest.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import dynamic from 'next/dynamic'
+// import React from 'react'
+// import dynamic from 'next/dynamic'
 
-function HomePage() {
-  const Map = React.useMemo(() => dynamic(
-    () => import('../components/LeafletMap'), // replace '@components/map' with your component's location
-    { 
-      loading: () => <p>A map is loading</p>,
-      ssr: false // This line is important. It's what prevents server-side render
-    }
-  ), [/* list variables which should trigger a re-render here */])
-  return <Map />
+// function HomePage() {
+//   const Map = React.useMemo(() => dynamic(
+//     () => import('../components/LeafletMap'), // replace '@components/map' with your component's location
+//     { 
+//       loading: () => <p>A map is loading</p>,
+//       ssr: false // This line is important. It's what prevents server-side render
+//     }
+//   ), [/* list variables which should trigger a re-render here */])
+//   return <Map />
+// }
+
+// export default HomePage
+
+import React from 'react'
+import DadataSuggest from '../components/DadataSuggest';
+const Test = () => {
+ return (
+	<DadataSuggest/>
+ )
 }
 
-export default HomePage
+export default Test;
