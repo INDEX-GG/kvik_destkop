7fdee435210fd9b9f7a9fe85e85c9bf65f4a96e3
sergeyM <cerhlhgr2@gmail.com>
2021-09-29 15:39:00 +0500

    29.09.2021 2

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,7 @@ const Index = () => {
 	const [limitRenderPage, setLimitRanderPage] = useState(0);
 	const [lastIdAds ,setLastIdAds] = useState(0);
 	const limit = 10
-
+	console.log(lastIdAds)
 	useEffect(() => {
 		scrollAds(id, isAuth, page, limit, data, setData, setLastIdAds, setLimitRanderPage, setPage)
 	}, [page])
