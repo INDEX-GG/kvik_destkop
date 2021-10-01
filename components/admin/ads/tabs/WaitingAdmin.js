842683fd428bb11145306cfeca3127ca70f87163
sergeyM <cerhlhgr2@gmail.com>
2021-10-01 11:41:01 +0500

    01/10/ update Api Ivan

--- components/admin/ads/tabs/WaitingAdmin.js
+++ components/admin/ads/tabs/WaitingAdmin.js
@@ -1,21 +1,32 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BtnReject } from '../../../Modals';
 import { Dialog } from '@material-ui/core';
 import WaitingCard from '../card/WaitingCard';
+import axios from 'axios';
 
 function WaitingAdmin({offers}) {
    console.log(offers)
-   const [check, setCheck] = useState(false);
+   const [check, setCheck] = useState( false );
    const [offerId, setOfferId] = useState([]);
-   
+   console.log(check)
    const [openWaitForm, setOpenWaitForm] = useState(false);
    const handleWaitFormDialog = () => setOpenWaitForm(!openWaitForm);
 
    function getDataChild ({id, isCheck}) {
-      setOfferId(isCheck ?  ( previous => [...previous, id] )  :  ( previous => previous.filter( item => item !== id) ) );
+      setOfferId( isCheck ?  previous => [...previous, id] : previous => previous.filter( item => item !== id) );
    }
+
+   const fetchApprove = () => {
+      axios.post("/api/verifyActive", {
+         "id": offerId,
+         "verify": "0",
+      })
+      .then( (responce) => console.log(responce))
+      console.log("nazat")
+   }
+   
    useEffect(() => {
-      offers.length === offerId.length ? /* check ? null : */ setCheck(true) : /* check === false ? null : */ setCheck(false);
+      offerId.length === offers.length ? check ? null : setCheck(true) : check === false ? null : setCheck(false);
    },[offerId])
 
    if(offers.length < 1) {
@@ -23,7 +34,7 @@ function WaitingAdmin({offers}) {
    }
    
    console.log("offerId=======>", offerId)
-   console.log("parentCheck=======>", check)
+   console.log("parentCheck=======>", offers.length)
 
    return (
       <div className="clientPage__container_bottom" >
@@ -31,22 +42,19 @@ function WaitingAdmin({offers}) {
             <label className="checkbox">
                <input 
                   type="checkbox"
-                  onChange={(event) => {
-                     setCheck(event.target.checked); 
-                     event.target.checked ? null : setOfferId([])
-                  }}
+                  onChange={ (event) => { setCheck(event.target.checked); event.target.checked === false ? setOfferId([]) : null }}
                   checked={check}
                />
                <div className="checkbox__text"></div>
             </label>
-            <a>Одобрить</a>
+            <a onClick={fetchApprove}>Одобрить</a>
          </div>
          <div className="clientPage__container_content">
             <div className="ads__container">
                {offers.map((offer, index) => {
                   return (
                      <WaitingCard 
-                        key={index}
+                        key={offer.id}
                         index={index}
                         offer={offer}
                         openWaitForm={openWaitForm}
