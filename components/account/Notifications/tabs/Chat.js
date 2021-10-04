0c999d1d6de9fc294a1809e8c6ff9305ac45cf89
Родион Пятигорский <pyatigorskyi@kvikdomain.local>
2021-10-04 14:57:55 +0500

    end

--- components/account/Notifications/tabs/Chat.js
+++ components/account/Notifications/tabs/Chat.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useAuth } from '../../../../lib/Context/AuthCTX'
 import {io} from 'socket.io-client';
-import axios from 'axios';
+// import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useStore } from '../../../../lib/Context/Store';
 
 
-let sender = {"id": 50, "name": "Станислав Даль"}
-let recipient = {"id": 51}
-let product = {"id": 70}
+// let sender = {"id": 50, "name": "Станислав Даль"}
+// let recipient = {"id": 51}
+// let product = {"id": 70}
 
 //? Говорим, на каком домене будем обслуживать сокерт
 // const socket = io('https://onekvik.ru', {path: "/cc/socket.io"})
@@ -14,29 +16,28 @@ const socket = io('http://192.168.8.111:6066', {path: "/socket.io"})
 // const socket = io('http://127.0.0.1:5000')
 
 
-socket.emit('join', {'sender': sender, 'recipient': recipient, 'product': product})
 
-const Chat = () => {
+const Chat = ({usersData: {sender, recipient, product}, messageData}) => {
 	const [message, setMessage] = useState('');
 	const [msgList, setMsgList] = useState([]);
+	const {query} = useRouter()
 	const refChat = useRef()
 	const refInput = useRef()
+	const {userInfo} = useStore()
 
 	const {id} = useAuth()
 
+	useEffect(() => {
+		console.log(messageData)
+	}, [messageData])
 
-	if (id) {
-		// axios.post('http://192.168.8.111:6066/chat_last_messages', {'user_id': 84}, {
-		// 	headers: {
-		// 		'Access-Control-Allow-Origin': '*',
-		// 	},
-		// 	proxy: {
-		// 		host: '192.168.8.111',
-		// 		port: 6066
-		// 	}
-		// }).then(r => console.log(r.data))
-		axios.post('http://192.168.8.111:6066/chat_last_messages', {'user_id': 84}).then(r => console.log(r.data))
-	}
+	useEffect(() => {
+		if (query?.customer_id && query?.seller_id && query?.product_id && userInfo?.name && id) {
+			console.log('join')
+			// socket.disconnect()
+			socket.emit('join', {'sender': sender, 'recipient': recipient, 'product': product})
+		}
+	}, [query, userInfo, id])
 
 	useEffect(() => {
 		if (refChat.current) {
@@ -44,12 +45,25 @@ const Chat = () => {
 		}
 	}, [msgList])
 
-	const handleSend = async () => {
+	useEffect(() => {
+		setMsgList([])
+	}, [query])
+
+ 	const handleSend = async () => {
 		if (message.length > 0) {
 			let data = new Date()
 			const messageDate = `${data.getHours()}:${data.getMinutes() > 9 ? data.getMinutes() : `0${data.getMinutes()}`}`
 			//? Событие оправки между клиентом и сервером
 			//! Убрать дату
+			// const sendObj = {
+			// 	'delete': false, 
+			// 	'message': message, 
+			// 	'messages_is_read': false, 
+			// 	'recipient': recipient.id, 
+			// 	'sender': recipient,
+			// 	'product': product, 
+			// 	'time': messageDate
+			// }
 			await socket.emit('text', {'message': message, 'sender': sender, 'recipient': recipient, 'product': product, 'date': messageDate})
 			setMessage('')
 		}
@@ -73,7 +87,6 @@ const Chat = () => {
 				if (data.sender?.id != id) {
 					socket.emit('online', {'sender': sender, 'recipient': recipient, 'product': product})
 				}
-
 				setMsgList(prev => [...prev, data])
 			}
 		})
@@ -104,25 +117,14 @@ const Chat = () => {
 	return (
 		<>
 			<div ref={refChat} className="messageChats">
-                {/* <div className="messageChat">
-                  <div className="chatDate small light">00.00.00</div>
-                  <div className="chatLocutor">
-                    <img src="https://source.unsplash.com/random?portrait" />
-                    <div>ТекстТекстТекстТекстТексТекстТекст</div>
-                    <div>00:00</div>
-                  </div>
-                  <div className="chatUser">
-                    <div>ТекстТе кстТек стТекс тТе ксТекстТекст ТекстТе кстТек стТекс тТе ксТекстТекст ТекстТе кстТек стТекс тТе ксТекстТекст ТекстТе кстТек стТекс тТе ксТекстТекст</div>
-                    <div>00:00</div>
-                  </div>
-                </div> */}
 				{msgList.map((item, index) => {
-					const myMessage = item.sender.id == 50
+					const myMessage = item.sender.id == id
 					return (
+						item?.delete ? null :
 						<div key={index} className={myMessage ? "chatUser" : "chatLocutor"}>
 							{myMessage ? null : <img src="https://source.unsplash.com/random?portrait" />}
 							<div>{item.message}</div>
-							<div>{item.date}</div>
+							<div>{item.tiem}</div>
 						</div>
 					)
 				})}
