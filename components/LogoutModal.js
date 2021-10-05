6df957d95d276387d3aaf10ef7acba66908262bb
narf200 <narf200@gmail.com>
2021-10-05 13:31:45 +0500

    отрефакторил модальное окно выхода из админки на mui

--- components/LogoutModal.js
+++ components/LogoutModal.js
@@ -1,60 +1,51 @@
-import * as React from 'react';
+import React from 'react'
 import {Button, Box, CardActions, CardContent, Typography, makeStyles} from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
     logout_wrapper: {
-        height: "100%",
-        display: "flex",
-        alignItems: "center",
+        display: 'flex',
+        alignItems: 'center',
+        width: '350px',
+        height: '250px',
+        padding: '8px 12px',
     },
     logout__block__title: {
         textAlign: 'center',
-        fontWeight: "500",
-        fontSize: "18px",
-        color: "$tp",
-        margin: "0",
-        marginBottom: "32px",
+        fontWeight: '500',
+        fontSize: '18px',
+        color: '#2c2c2c',
+        marginBottom: '32px',
     },
     modal__block_btn: {
         width: '100%',
         display: 'flex',
         justifyContent: 'spaceBetween',
-        padding: '0 20px',
-        fontWeight: '500',
-        fontSize: '18px',
     },
     btn_blue__accountLogoutButtonYes: {
-        color: '$prime',
+        color: '#00A0ABFF',
+        fontSize: '18px',
     },
     btn_red__accountLogoutButtonYes: {
-        color: '$primered',
+        color: '#F44545FF',
+        fontSize: '18px',
     }
 }))
 
-export default function LogoutModal(){
+const LogoutModal = () => {
     const classes = useStyles();
 
     return (
         <Box className={classes.logout_wrapper}>
                 <CardContent className={classes.logout__block__title}>
-                    <Typography variant="h6" >
-                        Вы уверены
-                    </Typography>
-                    <Typography variant="h6" >
-                        что хотите
-                    </Typography>
-                    <Typography variant="h6" >
-                        выйти?
-                    </Typography>
+                    <Typography className={classes.logout__block__title}>Вы уверены что хотите выйти?</Typography>
                 </CardContent>
             <Box className={classes.modal__block_btn}>
             <CardActions>
-                <Button className={classes.btn_blue__accountLogoutButtonYes}>ОТМЕНА</Button>
-            </CardActions>
-            <CardActions>
-                <Button   className={classes.btn_red__accountLogoutButtonYes}>ВЫЙТИ</Button>
+                <Button className={classes.btn_blue__accountLogoutButtonYes} href="">ОТМЕНА</Button>
+                <Button   className={classes.btn_red__accountLogoutButtonYes} href="">ВЫЙТИ</Button>
             </CardActions>
             </Box>
         </Box>
     );
 }
+export default LogoutModal
