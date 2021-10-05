89aa6ccaec6d7debc950442ba486a3348da80e0e
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-10-05 14:45:09 +0500

    05.10.21

--- pages/api/citySearch2.js
+++ pages/api/citySearch2.js
@@ -7,13 +7,11 @@ export default async function handler(req, res) {
             const limit = 10
             const lowername = name.toLowerCase()
             let cities = await prisma.$queryRaw(`SELECT region, municipality, settlement, type, latitude_dd, longitude_dd, unical_in_country, unical_in_region FROM russian_cities WHERE LOWER (settlement) LIKE '${lowername}%' LIMIT ${limit}`)
-            console.log(cities.length)
             if (cities.lenght === limit) {
                 return cities
             } else {
                 const partLimit = limit - cities.length
                 const citiesMiddle = await prisma.$queryRaw(`SELECT region, municipality, settlement, type, latitude_dd, longitude_dd, unical_in_country, unical_in_region FROM russian_cities WHERE LOWER (settlement) LIKE '%${lowername}%' AND LOWER (settlement) NOT LIKE '${lowername}%' LIMIT ${partLimit}`)
-                cities.push(123)
                 for (let city of citiesMiddle) {
                     cities.push(city)
                 }
