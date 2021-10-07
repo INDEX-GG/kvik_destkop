4ba7163480fb214aa0beb451b39f83c63fdce3cf
sergeyM <cerhlhgr2@gmail.com>
2021-10-07 12:29:13 +0500

    07.10.2021

--- components/product/ProductNumberPng.js
+++ components/product/ProductNumberPng.js
@@ -5,10 +5,9 @@ const ProductNumberPng = ({name, x, y}) => {
 
 	const canvasRef = useRef()
 	const [img, setImg] = useState('')
-
 	useEffect(async () => {
 		let canvasTxt = canvasRef.current.getContext("2d");
-		canvasTxt.canvas.width = 207;
+		canvasTxt.canvas.width = 215;
 		canvasTxt.canvas.height = 36;
 		canvasTxt.font = 'bold 30px sans-serif'
 		canvasTxt.fillText(name, x, y);
