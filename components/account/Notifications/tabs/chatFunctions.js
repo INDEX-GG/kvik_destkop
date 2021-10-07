09e7053fdbcb7d4a5622c31d8809d31c66194535
Родион Пятигорский <pyatigorskyi@kvikdomain.local>
2021-10-07 15:53:20 +0500

    change

--- components/account/Notifications/tabs/chatFunctions.js
+++ components/account/Notifications/tabs/chatFunctions.js
@@ -1,3 +1,6 @@
+import axios from "axios"
+import { CHAT_URL_API } from "../../../../lib/constants"
+
 export const generateTime = (UTC, time, onlyTime=false, onlyDate=false) => {
 	const dateObj = JSON.parse(time)
 	if (onlyDate) return `${dateObj.d}.${dateObj.mo}.${dateObj.y}`
@@ -9,3 +12,10 @@ export const generateProductPhoto = (photos) => {
 	const photosArr = JSON.parse(photos)?.photos
 	return photosArr[0]
   }
+
+export const generateDataTocken = (id, token) => {
+	token.then((r) => {
+		const obj = {"user_id": id, "platform": 3, "token": r}
+		console.log(obj)
+		axios.post(`${CHAT_URL_API}/push_token`, obj)})
+}
