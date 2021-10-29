a35efb6f30071d250339d5ede62c78a82fe5d62f
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-10-29 12:06:30 +0500

    29.10.21

--- pages/api/getProductOfUser.js
+++ pages/api/getProductOfUser.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
 		const main = async() => {
 			//Этот запрос нужно будет связать с таблицей
 			const getPost = async(ids) => {
-				return await prisma.$queryRaw(`SELECT posts.archived,users."userPhoto",posts.secure_transaction,posts.description,posts.id,posts.category_id,posts.price,posts.photo,posts.rating,posts.created_at,posts.delivery,posts.reviewed,posts.address,posts.phone,posts.trade,posts.verify, posts.verify_moderator, posts.active,posts.title,posts.email FROM "posts","users" WHERE (user_id = ${ids}) AND (users.id = user_id)`)
+				return await prisma.$queryRaw(`SELECT posts.archived,users."userPhoto",posts.secure_transaction,posts.description,posts.id,posts.category_id,posts.price,posts.photo,posts.rating,posts.created_at,posts.delivery,posts.reviewed,posts.address,posts.phone,posts.trade,posts.verify, posts.verify_moderator, posts.active,posts.title,posts.email FROM "posts","users" WHERE (user_id = ${ids}) AND (users.id = user_id) AND posts.active = 0 AND posts.verify = 0`)
 			}
 			return await getPost(+req.body.user_id);
 		}
