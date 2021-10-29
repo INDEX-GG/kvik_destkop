cb9930aa1cc32f7a4580f0c92a180ab9b9649419
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-10-29 17:58:34 +0500

    29.10.21

--- pages/api/getPostsCheck.js
+++ pages/api/getPostsCheck.js
@@ -5,10 +5,32 @@ export default async function handler(req, res) {
         const prisma = new PrismaClient();
 
         const main = async () => {
-            const data = req.body.data.toLowerCase();
+            const category = req.body.category.toLowerCase();
+            const text = req.body.text.toLowerCase();
             const page_limit = req.body.page_limit
             const page = (req.body.page - 1) * page_limit
-            return await prisma.$queryRaw(`SELECT * FROM posts WHERE LOWER (category_id) LIKE '${data}%' AND active = 0 AND verify = 0 ORDER BY id desc LIMIT ${page_limit} offset ${page}`)
+            const price_min = req.body.price.min
+            const price_max = req.body.price.max
+            const check = req.body.check
+            console.log(check);
+            console.log('-- -- --')
+            let constructQuery = ''
+            for (const [key, value] of Object.entries(check)) {
+                console.log(`${key}: ${value}`);
+                if (typeof value === 'object') {
+                    console.log("it`s Object!")
+                    constructQuery.concat()
+                } else {
+                    console.log("it`s String!")
+                    constructQuery =  constructQuery.concat(" AND LOWER (", category, ".", key, ") = '", value.toLowerCase(), "'")
+                }
+            }
+            console.log('--- --- ---')
+            console.log(constructQuery);
+
+
+            return await prisma.$queryRaw(`SELECT posts.archived,posts.secure_transaction,posts.description,posts.id,posts.category_id,posts.price,posts.photo,posts.rating,posts.created_at,posts.delivery,posts.reviewed,posts.address,posts.phone,posts.trade,posts.verify, posts.verify_moderator, posts.active,posts.title,posts.email FROM "posts","${category}" WHERE (posts.id = ${category}.post_id) AND posts.active = 0 AND posts.verify = 0 AND posts.price >= ${price_min} AND posts.price <= ${price_max} ${constructQuery} AND (LOWER (title) LIKE '%${text}%' OR LOWER (description) LIKE '%${text}%') ORDER BY id desc LIMIT ${page_limit} offset ${page}`)
+
 
         }
 
