fa5b63b09761ed40cf9a1309a0ba7b4ebcad7a90
narf200 <narf200@gmail.com>
2021-11-01 17:35:50 +0500

    feat: add Reviews placeholder.

---
+++ components/placeHolders/ReviewPlaceHolder/ReviewLeftPlaceHolder/ReviewLeftPlaceHolder.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {Box, Container} from "@material-ui/core";
+import {Grid, Skeleton} from "@mui/material";
+
+
+
+const CardBoxItem = () => {
+    return (
+        <Grid item container xs={12} spacing={1}>
+            {/*левый квадрат*/}
+            <Grid item xs={12}>
+                <Box>
+                    <Skeleton animation="wave" variant="rectangular" sx={{bgcolor: '#C7C7C780', borderRadius: '15px'}}>
+                        <div style={{width: "888px", height: "112px"}}/>
+                    </Skeleton>
+                </Box>
+            </Grid>
+        </Grid>
+    )
+}
+
+const CardBigBoxItem = () => {
+    return (
+        <Grid item container xs={12} spacing={1}>
+            {/*левый квадрат*/}
+            <Grid item xs={12}>
+                <Box>
+                    <Skeleton animation="wave" variant="rectangular" sx={{bgcolor: '#C7C7C780', borderRadius: '15px'}}>
+                        <div style={{width: "888px", height: "183px"}}/>
+                    </Skeleton>
+                </Box>
+            </Grid>
+        </Grid>
+    )
+}
+
+
+const ReviewLeftPlaceHolder = () => {
+    return (
+
+        <Container>
+            <Grid container xs={12}  spacing={2}>
+                {/*Обьявления*/}
+                <Grid item container xs={12} spacing={2}>
+                    <Grid item xs={12}>
+                        <Box>
+                            <Skeleton animation="wave" variant="rectangular"
+                                      sx={{bgcolor: '#C7C7C780', borderRadius: '15px'}}>
+                                <div style={{width: "106px", height: "16px"}}/>
+                            </Skeleton>
+                        </Box>
+                    </Grid>
+
+                    <CardBoxItem />
+                    <CardBoxItem />
+                    <CardBoxItem />
+                    <CardBigBoxItem />
+
+                </Grid>
+
+            </Grid>
+        </Container>
+    );
+};
+
+export default ReviewLeftPlaceHolder;
