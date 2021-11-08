78ced28fec84a35c5888675ccf5ce05b6cb3b941
narf200 <narf200@gmail.com>
2021-11-08 15:31:43 +0500

    fix: delete test(mock) searches № 72

--- components/account/Favorites/tabs/Searches.js
+++ components/account/Favorites/tabs/Searches.js
@@ -5,7 +5,7 @@ import FavoritesSearchesPlaceHolder
 	from "../../../placeHolders/FavoritesPlaceHolder/FavoritesSearchesPlaceHolder/FavoritesSearchesPlaceHolder";
 
 function Searches(data) {
-	if (data.searches.lenght == 0) {
+	if (data.searches.length === 0) {
 		return (
 			<>
 				{!data ? <FavoritesSearchesPlaceHolder/>
@@ -25,26 +25,31 @@ function Searches(data) {
 			setCardId( isCheck ? prev => [...prev, id] : prev => prev.filter( item => item !== id) )
 	}
 
+
 	return (
 		<>
 			{!data ? <FavoritesSearchesPlaceHolder/>
 				: <div className="clientPage__container_bottom">
-						<div className="clientPage__container_nav__radio">
-							<label className="checkbox">
-								<input
-									type="checkbox"
-									onChange={(event) => {setCheck(event.target.checked); event.target.checked ? null : setCardId([])}}
-									checked={check}
-								/>
-								<div className="checkbox__text"></div>
-							</label>
-							<a className="small light underline" style={dataCardId.length > 0 ? {color: "black"} : null} onClick={() => {
-									if (dataCardId.length > 0){
-										///
-									}
-							}}>Удалить</a>
-							<a className="clientPage__container_nav__radio_end">Получать на почту:</a>
-						</div>
+					{data.searches.length > 1 && <div className="clientPage__container_nav__radio">
+						<label className="checkbox">
+							<input
+								type="checkbox"
+								onChange={(event) => {
+									setCheck(event.target.checked);
+									event.target.checked ? null : setCardId([])
+								}}
+								checked={check}
+							/>
+							<div className="checkbox__text"/>
+						</label>
+						<a className="small light underline" style={dataCardId.length > 0 ? {color: "black"} : null}
+						   onClick={() => {
+							   if (dataCardId.length > 0) {
+								   ///
+							   }
+						   }}>Удалить</a>
+						<a className="clientPage__container_nav__radio_end">Получать на почту:</a>
+					</div>}
 						<div className="clientPage__container_content">
 							<div className="searchesWrapper">
 								{data.searches.map((search) => {
