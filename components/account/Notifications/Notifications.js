5d018c99d4c4957ea67d9c787d27e0bee3b281d7
narf200 <narf200@gmail.com>
2021-11-08 09:14:08 +0500

    fix: remove test notifications

--- components/account/Notifications/Notifications.js
+++ components/account/Notifications/Notifications.js
@@ -22,31 +22,31 @@ const dialogsBox = [
 
 // Уведомления
 const notifBox = [
-  {
-    id: 1,
-    date: "Сегодня",
-    mess: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit sagittis elit, in vestibulum mauris. Phasellus ante mi, finibus sit amet massa sed, sagittis cursus lacus. Aenean eget eros molestie, tempor lacus ac, accumsan ante. Duis pellentesque turpis ut diam ultricies fringilla. Aliquam vitae porta dolor. Mauris gravida, neque vel scelerisque volutpat, ex mi scelerisque nunc, ut bibendum nulla risus quis neque. Suspendisse id elit et ex placerat tincidunt. Vestibulum ultricies rutrum aliquet. Sed sit amet tellus mattis, scelerisque lacus in, sodales elit. Nunc sit amet augue ultrices, auctor est eu, finibus dui. Suspendisse auctor viverra rutrum.",
-    time: "00:00",
-  },
-  {
-    id: 2,
-    date: "Вчера",
-    mess: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam rutrum lectus quis iaculis pretium. Proin a turpis vehicula, consequat neque eu, ullamcorper lorem. Ut ullamcorper ipsum non ante pretium, eget laoreet velit suscipit. Duis non lectus neque. Fusce commodo volutpat massa, quis finibus nulla. Sed eu lacus malesuada, euismod lorem eget, bibendum risus. Fusce ultrices augue vel blandit blandit. Nulla dignissim lorem ipsum, quis lobortis est finibus dapibus.",
-    time: "00:00",
-  },
-  {
-    id: 3,
-    date: "06 апреля 2021",
-    mess: "Pellentesque tempor eros ac scelerisque porta. Donec mi purus, pulvinar et neque eget, maximus elementum felis. Vivamus viverra metus id erat lacinia placerat. Proin auctor ante id ex dictum posuere. Donec a lorem quam. Proin mollis facilisis porta. Sed bibendum, elit bibendum ullamcorper congue, ligula turpis pellentesque quam, non luctus ipsum nulla vitae sapien. ",
-    time: "00:00",
-  },
-  { id: 4, date: "05 апреля 2021", mess: "Fusce ac euismod augue. Nullam lobortis facilisis semper. Ut elementum augue eget sem viverra, non placerat urna accumsan. Pellentesque convallis massa eu tincidunt fermentum.", time: "00:00" },
-  {
-    id: 5,
-    date: "04 апреля 2021",
-    mess: "Aenean hendrerit a magna vitae commodo. Aliquam tristique pellentesque sem. Ut vitae elit ligula. Sed quis libero quis magna convallis dignissim. Sed aliquet, libero sit amet luctus volutpat, massa odio pulvinar ligula, in ullamcorper risus leo tempor felis. In ac erat in augue mattis scelerisque finibus nec sem. Vivamus nec neque ultrices, ultrices sem nec, commodo felis. Proin elit metus, egestas at urna ac, congue gravida velit. Mauris ac lectus mauris.",
-    time: "00:00",
-  },
+  // {
+  //   id: 1,
+  //   date: "Сегодня",
+  //   mess: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit sagittis elit, in vestibulum mauris. Phasellus ante mi, finibus sit amet massa sed, sagittis cursus lacus. Aenean eget eros molestie, tempor lacus ac, accumsan ante. Duis pellentesque turpis ut diam ultricies fringilla. Aliquam vitae porta dolor. Mauris gravida, neque vel scelerisque volutpat, ex mi scelerisque nunc, ut bibendum nulla risus quis neque. Suspendisse id elit et ex placerat tincidunt. Vestibulum ultricies rutrum aliquet. Sed sit amet tellus mattis, scelerisque lacus in, sodales elit. Nunc sit amet augue ultrices, auctor est eu, finibus dui. Suspendisse auctor viverra rutrum.",
+  //   time: "00:00",
+  // },
+  // {
+  //   id: 2,
+  //   date: "Вчера",
+  //   mess: "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam rutrum lectus quis iaculis pretium. Proin a turpis vehicula, consequat neque eu, ullamcorper lorem. Ut ullamcorper ipsum non ante pretium, eget laoreet velit suscipit. Duis non lectus neque. Fusce commodo volutpat massa, quis finibus nulla. Sed eu lacus malesuada, euismod lorem eget, bibendum risus. Fusce ultrices augue vel blandit blandit. Nulla dignissim lorem ipsum, quis lobortis est finibus dapibus.",
+  //   time: "00:00",
+  // },
+  // {
+  //   id: 3,
+  //   date: "06 апреля 2021",
+  //   mess: "Pellentesque tempor eros ac scelerisque porta. Donec mi purus, pulvinar et neque eget, maximus elementum felis. Vivamus viverra metus id erat lacinia placerat. Proin auctor ante id ex dictum posuere. Donec a lorem quam. Proin mollis facilisis porta. Sed bibendum, elit bibendum ullamcorper congue, ligula turpis pellentesque quam, non luctus ipsum nulla vitae sapien. ",
+  //   time: "00:00",
+  // },
+  // { id: 4, date: "05 апреля 2021", mess: "Fusce ac euismod augue. Nullam lobortis facilisis semper. Ut elementum augue eget sem viverra, non placerat urna accumsan. Pellentesque convallis massa eu tincidunt fermentum.", time: "00:00" },
+  // {
+  //   id: 5,
+  //   date: "04 апреля 2021",
+  //   mess: "Aenean hendrerit a magna vitae commodo. Aliquam tristique pellentesque sem. Ut vitae elit ligula. Sed quis libero quis magna convallis dignissim. Sed aliquet, libero sit amet luctus volutpat, massa odio pulvinar ligula, in ullamcorper risus leo tempor felis. In ac erat in augue mattis scelerisque finibus nec sem. Vivamus nec neque ultrices, ultrices sem nec, commodo felis. Proin elit metus, egestas at urna ac, congue gravida velit. Mauris ac lectus mauris.",
+  //   time: "00:00",
+  // },
 ];
 
 //Пагинация
