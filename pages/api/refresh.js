11f98dd94df31eaa6ef95b1c63f0559e76dd9f1f
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-11-18 16:38:35 +0500

    18.11.21

---
+++ pages/api/refresh.js
@@ -0,0 +1,40 @@
+import { sign } from 'jsonwebtoken'
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+
+        const main = async () => {
+            const jwt = require("jsonwebtoken");
+            const token = req.headers["x-access-token"];
+            if (!token) {
+                return res.status(403).send("A token is required for authentication");
+            }
+            try {
+                jwt.verify(token, process.env.NEXT_PUBLIC_JWT_REFRESH_SECRET);
+            } catch (err) {
+                return res.status(401).send("Invalid Token");
+            }
+            const tokenUser = jwt.verify(token, process.env.NEXT_PUBLIC_JWT_REFRESH_SECRET).sub
+            console.log(tokenUser);
+            const claims = {sub: tokenUser}
+            const new_jwt = sign(claims, process.env.NEXT_PUBLIC_JWT_SECRET, { expiresIn: 120})
+            return { authToken: new_jwt }
+
+        }
+
+        try {
+            let response = await main();
+            res.status(200);
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(response))
+        }
+        catch (e) {
+            console.error(`ошибка api refresh${e}`)
+            res.json('ошибка api refresh', e)
+            res.status(405).end();
+        }
+
+    } else {
+        res.json({ message: 'method not allowed' })
+        res.status(405).end()
+    }
+}
