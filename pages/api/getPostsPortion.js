177ada1475d98e73bcd73ad10cdc18d03e4def19
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-11-25 10:44:01 +0500

    25.11.21

--- pages/api/getPostsPortion.js
+++ pages/api/getPostsPortion.js
@@ -9,9 +9,35 @@ export default async function handler(req, res) {
             const page_limit = req.body.page_limit
             const page = (req.body.page - 1) * page_limit
             // const user_id = (req.body.user_id).toString()
+            const sort = req.body.sort.toLowerCase()
+            const region_includes = req.body.region_includes.toLowerCase()
+            let region_excludes = req.body.region_excludes.toLowerCase()
+            if (region_excludes === '') {
+                region_excludes = '!'
+            }
+            let sort_value
+            switch (sort) {
+                case 'default':
+                    sort_value = 'ORDER BY id desc'
+                    break;
+                case 'new':
+                    sort_value = 'ORDER BY id desc'
+                    break;
+                case 'price_by_ascending':
+                    sort_value = 'ORDER BY price asc'
+                    break;
+                case 'price_by_descending':
+                    sort_value = 'ORDER BY price desc'
+                    break;
+                default:
+                    sort_value = ''
+                    break;
+            }
+            console.log(sort_value);
+            const answer  = await pool.query(`SELECT users.name AS user_name, users."userPhoto" AS user_photo, users.phone AS user_phone, users.raiting AS user_raiting, posts.id, posts.user_id, posts.category_id, posts.price, posts.old_price, posts.photo, posts.rating, posts.created_at, posts.delivery, posts.reviewed, posts.address, posts.phone, posts.trade, posts.verify_moderator, posts.commercial, posts.secure_transaction, posts.title, posts.email, posts.viewing, posts.city FROM "posts" INNER JOIN "users" ON posts.user_id = users.id WHERE active = 0 AND verify = 0  AND LOWER (city) LIKE '${region_includes}%' AND LOWER (city) NOT LIKE '%${region_excludes}' ${sort_value} LIMIT ${page_limit} offset ${page}`)
 
-            const answer  = await pool.query(`SELECT users.name AS user_name, users."userPhoto" AS user_photo, users.phone AS user_phone, users.raiting AS user_raiting, posts.id, posts.user_id, posts.category_id, posts.price, posts.old_price, posts.photo, posts.rating, posts.created_at, posts.delivery, posts.reviewed, posts.address, posts.phone, posts.trade, posts.verify_moderator, posts.commercial, posts.secure_transaction, posts.title, posts.email, posts.viewing FROM "posts" INNER JOIN "users" ON posts.user_id = users.id WHERE active = 0 AND verify = 0 ORDER BY id desc LIMIT ${page_limit} offset ${page}`)
 
+            // const answer  = await pool.query(`SELECT users.name AS user_name, users."userPhoto" AS user_photo, users.phone AS user_phone, users.raiting AS user_raiting, posts.id, posts.user_id, posts.category_id, posts.price, posts.old_price, posts.photo, posts.rating, posts.created_at, posts.delivery, posts.reviewed, posts.address, posts.phone, posts.trade, posts.verify_moderator, posts.commercial, posts.secure_transaction, posts.title, posts.email, posts.viewing FROM "posts" INNER JOIN "users" ON posts.user_id = users.id WHERE active = 0 AND verify = 0 ORDER BY id desc LIMIT ${page_limit} offset ${page}`)
             // for (let index in answer.rows) {
             //     let views = (answer.rows[index]).viewing
             //     let preList = views.substring(1);
