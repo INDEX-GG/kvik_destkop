a3141702815994e671d806c86023871d69cfe039
nettunovivi <96100787+nettunovivi@users.noreply.github.com>
2021-12-17 18:01:41 +0500

    chore: starting work

--- components/account/Favorites/tabs/Offers.js
+++ components/account/Favorites/tabs/Offers.js
@@ -1,110 +1,130 @@
-import React, {useState, useEffect} from "react";
-import EmptyPlaceholder from "../../../EmptyPlaceholder";
-import {Checkbox} from "@material-ui/core";
-import OfferFavorite from "../card/offerFavorite";
-import {useStore} from "../../../../lib/Context/Store";
-import {checkArray} from '../../../../lib/services'
-import {makeStyles} from "@material-ui/styles";
-import FiberManualRecordOutlinedIcon from '@material-ui/icons/FiberManualRecordOutlined';
-import FiberManualRecordSharpIcon from '@material-ui/icons/FiberManualRecordSharp';
-import FavoritesOffersPlaceHolder from "../../../placeHolders/FavoritesPlaceHolder/FavoritesActiveOfffer/FavoritesOffersPlaceHolder";
+import { useState, useEffect } from "react";
+import { Checkbox, makeStyles } from "@material-ui/core";
+import { FiberManualRecordOutlined, FiberManualRecordSharp } from "@material-ui/icons";
+import { useStore } from "#lib/Context/Store";
+import { checkArray } from "#lib/services";
+import OfferFavorite from "#components/account/Favorites/card/offerFavorite"
+import EmptyPlaceholder from "#components/EmptyPlaceholder"
+import FavoritesOffersPlaceHolder from "#components/placeHolders/FavoritesPlaceHolder/FavoritesActiveOfffer/FavoritesOffersPlaceHolder";
 
 
 const useStyles = makeStyles(() => ({
-    delete: {
-        fontSize: "16px !important",
-    },
-    deleteActiv: {
-        color: "black",
-    }
+	delete: {
+		fontSize: "16px !important",
+	},
+	deleteActiv: {
+		color: "black",
+	}
 }));
 
+/**
+ * @typedef OfferData
+ * @property {[]} itemsPost
+ */
 
-function Offers(data) {
-    const classes = useStyles();
-    const [check, setCheck] = useState(false);
-    const [deleteButton, setDeleteButton] = useState(false);
-    const [dataCheck, setDataCheck] = useState([]);
-    const {userInfo, setLikeCommentArray} = useStore();
+/**
+ * @param {OfferData} data 
+ */
+const Offers = (data) => {
+	const classes = useStyles();
+	const [check, setCheck] = useState(false);
+	const [deleteButton, setDeleteButton] = useState(false);
+	const [dataCheck, setDataCheck] = useState([]);
+	const { userInfo, setLikeCommentArray } = useStore();
 
-    function getChildCheck(childCheck) {
-        setDataCheck(childCheck.isChecked ? prev => [...prev, childCheck.id] : dataCheck => dataCheck.filter(item => item !== childCheck.id));
-    }
+	/**
+	 * @param {{ isChecked: boolean, id: string }} childCheck 
+	 */
+	const getChildCheck = (childCheck) => {
+		const someData = childCheck.isChecked
+			? (prev) => [...prev, childCheck.id]
+			: dataCheck => dataCheck.filter(item => item !== childCheck.id)
+		setDataCheck(someData);
+	}
 
-    const getFavoritsUser = (likeId) => {
-        let favoritesArray = [];
-        likeId.map((items) => {
-            let comment = checkArray(userInfo?.favorites) && (userInfo.favorites.filter(item => item.post_id === +items)[0])?.comment !== undefined ? (userInfo?.favorites.filter(item => item.post_id === +items)[0])?.comment : ''
-            let like = checkArray(userInfo?.favorites) && userInfo.favorites.filter(item => item.post_id === +items).map(item => item.condition).join() === 'false'
-            favoritesArray.push({
-                post_id: `${items}`,
-                comment: `${comment}`,
-                condition: `${like}`,
-            })
-        })
-        setLikeCommentArray(favoritesArray)
-    }
+	/**
+	 * @param {[]} likeId 
+	 */
+	const getFavoritsUser = (likeId) => {
+		let favoritesArray = [];
+		likeId.map((items) => {
+			let comment = checkArray(userInfo?.favorites) && (userInfo.favorites.filter(item => item.post_id === +items)[0])?.comment !== undefined ? (userInfo?.favorites.filter(item => item.post_id === +items)[0])?.comment : ''
+			let like = checkArray(userInfo?.favorites) && userInfo.favorites.filter(item => item.post_id === +items).map(item => item.condition).join() === 'false'
+			favoritesArray.push({
+				post_id: `${items}`,
+				comment: `${comment}`,
+				condition: `${like}`,
+			})
+		})
+		setLikeCommentArray(favoritesArray)
+	}
 
-    useEffect(() => {
-        dataCheck.length > 0 ? data.itemsPost.length === dataCheck.length ? setCheck(true) : setCheck(false) : null;
-    }, [dataCheck])
+	useEffect(() => {
+		dataCheck.length > 0 
+			? data.itemsPost.length === dataCheck.length 
+				? setCheck(true) 
+				: setCheck(false) 
+			: null;
+	}, [dataCheck])
 
-    if (data.itemsPost?.length === 0 || data.itemsPost?.length === undefined) {
-        return (
-            <>
-                {!userInfo ? <FavoritesOffersPlaceHolder/>
-                    : <EmptyPlaceholder
-                        title='Здесь будут ваши избранные объявления'
-                        subtitle='Нажмите на  сердечко 💙️, чтобы добавить объявление в избранное'
-                        img='/accountImage/OffersNone.png'
-                        imgAlt='offers_placholder'/>}
-            </>
-        );
-    }
+	if (data.itemsPost?.length === 0 || data.itemsPost?.length === undefined) {
+		return (
+			<>
+				{!userInfo 
+					? <FavoritesOffersPlaceHolder />
+					: <EmptyPlaceholder
+						title='Здесь будут ваши избранные объявления'
+						subtitle='Нажмите на  сердечко 💙️, чтобы добавить объявление в избранное'
+						img='/accountImage/OffersNone.png'
+						imgAlt='offers_placholder' />}
+			</>
+		);
+	}
 
-    return (
+	return (
 
-        <>
-            {!userInfo ? <FavoritesOffersPlaceHolder/>
-                : <div className="clientPage__container_bottom">
-                    <div className="clientPage__container_nav__radio">
-                        <Checkbox
-                            color="primary"
-                            onChange={(event) => {
-                                setCheck(!check);
-                                event.target.checked ? null : setDataCheck([])
-                            }}
-                            checked={check}
-                            icon={<FiberManualRecordOutlinedIcon/>}
-                            checkedIcon={<FiberManualRecordSharpIcon/>}
-                        />
-                        <a
-                            onClick={() => dataCheck.length > 0 ? (getFavoritsUser(dataCheck), setDataCheck([]), setCheck(false), setDeleteButton(!deleteButton)) : null}
-                            style={dataCheck.length > 0 ? {color: "black"} : null}
-                            className={classes.delete}
-                        >
-                            Удалить
-                        </a>
-                    </div>
-                    <div className="clientPage__container_content">
-                        <div className="favoritesContainerWrapper">
-                            {data.itemsPost?.map((offer, i) =>
-                                <OfferFavorite
-                                    offer={offer}
-                                    key={i}
-                                    i={i}
-                                    parentCheck={check}
-                                    getChildCheck={getChildCheck}
-                                    dataCheck={dataCheck}
-                                    deleteButton={deleteButton}
-                                />
-                            )}
-                        </div>
-                    </div>
-                </div>}
-        </>
+		<>
+			{!userInfo 
+				? <FavoritesOffersPlaceHolder />
+				: <div className="clientPage__container_bottom">
+					<div className="clientPage__container_nav__radio">
+						<Checkbox
+							color="primary"
+							onChange={(event) => {
+								setCheck(!check);
+								event.target.checked ? null : setDataCheck([])
+							}}
+							checked={check}
+							icon={<FiberManualRecordOutlined />}
+							checkedIcon={<FiberManualRecordSharp />}
+						/>
+						<a
+							onClick={() => dataCheck.length > 0 ? (getFavoritsUser(dataCheck), setDataCheck([]), setCheck(false), setDeleteButton(!deleteButton)) : null}
+							style={dataCheck.length > 0 ? { color: "black" } : null}
+							className={classes.delete}
+						>
+							Удалить
+						</a>
+					</div>
+					<div className="clientPage__container_content">
+						<div className="favoritesContainerWrapper">
+							{data.itemsPost?.map((offer, index) =>
+								<OfferFavorite
+									offer={offer}
+									key={index}
+									i={index}
+									parentCheck={check}
+									getChildCheck={getChildCheck}
+									dataCheck={dataCheck}
+									deleteButton={deleteButton}
+								/>
+							)}
+						</div>
+					</div>
+				</div>}
+		</>
 
-    );
+	);
 }
 
 export default Offers;
