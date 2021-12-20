4c00bc0ddcbe7aea9f139af895ec2b9c772b13e6
nettunovivi <96100787+nettunovivi@users.noreply.github.com>
2021-12-20 13:36:31 +0500

    fix(client): fav post delete check
    Новые компоненты:
    - InternalLink

--- components/account/Favorites/card/offerFavorite.js
+++ components/account/Favorites/card/offerFavorite.js
@@ -6,44 +6,55 @@ import { ToRubles, ToRusDate } from "../../../../lib/services";
 import { useStore } from "../../../../lib/Context/Store";
 import FiberManualRecordOutlinedIcon from '@material-ui/icons/FiberManualRecordOutlined';
 import FiberManualRecordSharpIcon from '@material-ui/icons/FiberManualRecordSharp';
+import { InternalLink } from '../../../links/InternalLink';
 
-
+InternalLink
 const useStyles = makeStyles(() => ({
 	check: {
 		position: "absolute",
 	},
 	toolTip: {
 		fontSize: "14px",
+	},
+	favImage: {
+	},
+	cardLink: {
+		width: "100%",
+		height: "100%"
+	},
+	cardImage: {
+		userSelect: "none",
+		maxHeight: "100%",
+		minWidth: "100%",
+		objectFit: "cover",
+		objectPosition: "center",
+		borderRadius: "10px",
 	}
 }));
 
 /**
  * @param {object} props 
  * @param {number} props.index
- * @param {import('../../../../lib/fetch').UserInfo} props.offer
+ * @param {import('../tabs/Offers').ItemPost} props.offer
  * @param {boolean} props.parentCheck
  * @param {import('../tabs/Offers').GetChildCheck} props.getChildCheck
  * @param {[]} props.dataCheck
  * @param {boolean} props.deleteButton
  */
-const OfferFavorite = ({index, offer, parentCheck, getChildCheck, dataCheck, deleteButton}) => {
-    const [check, setCheck] = useState(false);
-    const classes = useStyles();
-    const { setLikeComment } = useStore();
+const OfferFavorite = ({ index, offer, parentCheck, getChildCheck, dataCheck, deleteButton }) => {
+	const [check, setCheck] = useState(false);
+	const classes = useStyles();
+	const { setLikeComment } = useStore();
 
-		/**
-		 * @param {Event} event 
-		 */
-    function deleteNote(event) {
-			event.target.innerHTML = '';
-			console.log();
-			let like = true;
-			let comment = '';
-			setLikeComment(+event.target.id, comment, like)
-		}
-
-	
-
+	/**
+	 * @param {Event} event 
+	 */
+	function deleteNote(event) {
+		event.target.innerHTML = '';
+		let like = true;
+		let comment = '';
+		setLikeComment(+event.target.id, comment, like)
+	}
 
 	useEffect(() => {
 		parentCheck
@@ -63,53 +74,63 @@ const OfferFavorite = ({index, offer, parentCheck, getChildCheck, dataCheck, del
 	}, [parentCheck, deleteButton]);
 
 
-    return (
-        <a key={index} href={`/product/${offer.id}`} className="favoritesContainer boxWrapper">
-            <div className="favoritesImage">
-                <div className="favoritesPubCheck">
-                    <Checkbox
-                        className={classes.check}
-                        color="primary"
-                        onChange={(event) => {setCheck(!check); getChildCheck({isChecked: event.target.checked, id: offer.id})}}
-                        checked={check}
-                        icon={<FiberManualRecordOutlinedIcon />}
-					    checkedIcon={<FiberManualRecordSharpIcon />}
-                    />
-                </div>
-
-
-				{/*<a className="favoritesCompare">*/}
-				{/*</a>*/}
-
-                <a href="javascript:void(0);">
-                    <Favorits favId={offer.id} isAccountCard />
-                </a>
-                <img key={index} src={`${STATIC_URL}/${JSON.parse(offer.photo)?.photos[0]}`}onError={e => e.target.src = `${BASE_URL}/icons/photocard_placeholder.svg`} />
-                {offer.user_blocked && <div className="favoritesCause megaLight">Пользователь заблокирован</div>}
-            </div>
-            <div className="favoritesDescription">
-                <a href={`/user/${offer.user_id}`} className="favoritesUserBlock small">
-                    <div>
-                        <div className='favoritesDescriptionUserName'>{offer.user_name}</div>
-                        <div className="favoritesDatPub light DatPub__mobile">
-                            {" "}
-                            {ToRusDate(offer.created_at)}
-                        </div>
-                    </div>
-                    <img className="favoritesUserpic" src={`${STATIC_URL}/${offer.user_photo}`} onError={e => e.target.src = `${BASE_URL}/icons/photocard_placeholder.svg`}/>
-                </a>
-                <div className="favoritesMiddle">
-                    <div>{ToRubles(offer.price)}</div>
-                    <div>{offer.title}</div>
-                    <div className="thin small light">{offer.address}</div>
-                </div>
-                <div  id={offer.id} onClick={(e) => deleteNote(e)} className="favoritesNote" title={offer.comment}>
-                    {offer.comment}
-                </div>
-                <a className="favoritesButton buttonGrey small">Сообщить об изменении цены</a>
-            </div>
-        </a>
-    )
+	return (
+		<a key={index} className="favoritesContainer boxWrapper">
+			<div className="favoritesImage">
+				<div className="favoritesPubCheck">
+					<Checkbox
+						className={classes.check}
+						color="primary"
+						onChange={(event) => { setCheck(!check); getChildCheck({ isChecked: event.target.checked, id: offer.id }) }}
+						checked={check}
+						icon={<FiberManualRecordOutlinedIcon />}
+						checkedIcon={<FiberManualRecordSharpIcon />}
+					/>
+				</div>
+				<InternalLink 
+					href={{
+						pathname: "/product/[id]",
+						query: { id: offer.id}
+					}}
+					className={classes.cardLink}
+				>
+					{/*<a className="favoritesCompare">*/}
+					{/*</a>*/}
+					<a href="javascript:void(0);">
+						<Favorits favId={offer.id} isAccountCard />
+					</a>
+					<img 
+						key={index} 
+						className={classes.cardImage}
+						src={`${STATIC_URL}/${JSON.parse(offer.photo)?.photos[0]}`} 
+						onError={e => e.target.src = `${BASE_URL}/icons/photocard_placeholder.svg`} 
+					/>
+					{offer.user_blocked && <div className="favoritesCause megaLight">Пользователь заблокирован</div>}
+				</InternalLink>
+			</div>
+			<div className="favoritesDescription">
+				<a href={`/user/${offer.user_id}`} className="favoritesUserBlock small">
+					<div>
+						<div className='favoritesDescriptionUserName'>{offer.user_name}</div>
+						<div className="favoritesDatPub light DatPub__mobile">
+							{" "}
+							{ToRusDate(offer.created_at)}
+						</div>
+					</div>
+					<img className="favoritesUserpic" src={`${STATIC_URL}/${offer.user_photo}`} onError={e => e.target.src = `${BASE_URL}/icons/photocard_placeholder.svg`} />
+				</a>
+				<div className="favoritesMiddle">
+					<div>{ToRubles(offer.price)}</div>
+					<div>{offer.title}</div>
+					<div className="thin small light">{offer.address}</div>
+				</div>
+				<div id={offer.id} onClick={(e) => deleteNote(e)} className="favoritesNote" title={offer.comment}>
+					{offer.comment}
+				</div>
+				<a className="favoritesButton buttonGrey small">Сообщить об изменении цены</a>
+			</div>
+		</a>
+	)
 }
 
 export default OfferFavorite;
