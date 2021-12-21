79b5bf7fc6b029b5a3215da808e01b72f3f79828
nettunovivi <96100787+nettunovivi@users.noreply.github.com>
2021-12-21 13:41:12 +0500

    style: empty offer placeholder

--- components/account/Offers/tabs/ArchiveIco/ArchiveIco.js
+++ components/account/Offers/tabs/ArchiveIco/ArchiveIco.js
@@ -1,18 +1,33 @@
 import React from 'react';
 
 const ArchiveIco = () => {
-    return (
-        <>
-            <svg width="167" height="158" viewBox="0 0 167 158" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M11.0917 58.0791H1L17.315 156.556H166L159.13 118.608M11.0917 58.0791H148.171L159.13 118.608M11.0917 58.0791V46.7888H158.095L159.13 118.608M23.5382 46.7888V34.5576H51.7951" stroke="#8F8F8F"/>
-                <path d="M51.7949 46.7885V1H150.862V46.7885" stroke="#8F8F8F"/>
-                <path d="M150.863 35.498H166.001V155.615" stroke="#8F8F8F"/>
-                <path d="M64.2422 36.7529H140.267" stroke="#8F8F8F"/>
-                <path d="M63.9062 25.1489H140.603" stroke="#8F8F8F"/>
-                <path d="M63.5684 14.6426H140.602" stroke="#8F8F8F"/>
-            </svg>
-        </>
-    );
+	return (
+		<>
+			<svg width="202" height="182" viewBox="0 0 202 182" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<g filter="url(#filter0_d_5015_49571)">
+					<path d="M20 60.4491V135.04L32.759 81.0598C35.2454 75.5636 41.9739 72.6628 45.0273 71.8995V49.8165H31.1233C23.4024 49.8165 20.4907 56.9049 20 60.4491Z" fill="#E9E9E9" />
+					<path d="M157.078 71.8995V64.0478C165.845 61.5614 170 68.2463 170.982 71.8995H157.078Z" fill="#E9E9E9" />
+					<path d="M148.899 22.172V71.8995H53.0426C53.1517 57.5047 53.3043 27.4065 53.0426 22.172C52.7809 16.9375 58.3861 15.956 61.2215 16.1196H141.701C148.375 15.2036 149.281 19.7729 148.899 22.172Z" fill="#E9E9E9" />
+					<path d="M37.9935 89.0751L26.0524 148.781C28.0153 157.025 33.5224 158.323 36.0306 157.941H157.078C163.621 158.465 167.001 153.252 167.874 150.58L179.815 90.7109C184.657 80.3728 175.944 78.9878 170.982 79.5876H53.0426C42.3119 78.4099 38.5388 85.4219 37.9935 89.0751Z" fill="#E9E9E9" />
+					<rect x="67.1289" y="31.8234" width="68.7024" height="8.17886" rx="4.08943" fill="white" />
+					<rect x="67.1289" y="48.1804" width="68.7024" height="8.17886" rx="4.08943" fill="white" />
+				</g>
+				<defs>
+					<filter id="filter0_d_5015_49571" x="0" y="0" width="201.232" height="182" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+						<feFlood floodOpacity="0" result="BackgroundImageFix" />
+						<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+						<feOffset dy="4" />
+						<feGaussianBlur stdDeviation="10" />
+						<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0" />
+						<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_5015_49571" />
+						<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_5015_49571" result="shape" />
+					</filter>
+				</defs>
+			</svg>
+
+
+		</>
+	);
 };
 
 export default ArchiveIco;
