372716231c57b02633dd27f0c59e58e516da941e
karimovmaxdev <karimov.maxdev@gmail.com>
2021-12-23 11:22:21 +0500

    исправление импорта

--- components/SortItem.js
+++ components/SortItem.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
-import {makeStyles, MenuItem, TextField} from "@material-ui/core";
+import {makeStyles, MenuItem, TextField, } from "@material-ui/core";
+// import SortItemIcon from '#UI/icons/SortItemIcon';
 
 
 const useStyles = makeStyles(() => ({
