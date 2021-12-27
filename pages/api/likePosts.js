1d16fa5ce0f932552ed352f1de79e4c474d23300
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-12-27 17:42:32 +0500

    27.12.21

--- pages/api/likePosts.js
+++ pages/api/likePosts.js
@@ -2,6 +2,22 @@ import {Pool} from "pg"
 let format = require('pg-format')
 export default async function handler(req, res) {
     if (req.method === 'POST') {
+
+        const jwt = require("jsonwebtoken");
+        const token = req.headers["x-access-token"];
+        if (!token) {
+            return res.status(403).send("A token is required for authentication");
+        }
+        try {
+            jwt.verify(token, process.env.NEXT_PUBLIC_JWT_SECRET);
+        } catch (err) {
+            return res.status(401).send("Invalid Token");
+        }
+        const tokenUser = jwt.verify(token, process.env.NEXT_PUBLIC_JWT_SECRET).sub
+        if (parseInt(req.body.user_id, 10) !== tokenUser) {
+            return res.status(403).send("Invalid Token");
+        }
+
         const pool = new Pool({ connectionString: process.env.DATABASE_URL })
         const main = async () => {
             let unlike_posts  = await pool.query(`DELETE FROM "favorites" WHERE user_id = ${req.body.user_id} AND liked_post_id IN (${[...new Set(req.body.posts)]}) RETURNING liked_post_id`)
