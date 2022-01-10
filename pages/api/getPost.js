1143c8ea20eab36f7b71f5f56d238e513ccfad1d
Sergey Zinyaev <zinyaevsi@gmail.com>
2022-01-10 17:47:11 +0500

    10.01.21

--- pages/api/getPost.js
+++ pages/api/getPost.js
@@ -5,18 +5,29 @@ export default async function handler(req, res) {
 	if (req.method === 'POST') {
 		const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 		const main = async () => {
+			if (typeof req.body.id !== 'number') {
+				return("err")
+			}
 			const answer  = await pool.query(`SELECT users."userPhoto",users.name, posts.user_id ,users.raiting, users.id,posts.secure_transaction,posts.description,posts.id,posts.category_id,posts.price,posts.photo,posts.rating,posts.created_at,posts.delivery,posts.reviewed,posts.address,posts.phone,posts.trade,posts.verify_moderator,posts.title,posts.email,posts.active, posts.subcategory, posts.viewing, posts.coordinates FROM "posts" INNER JOIN "users" ON posts.user_id = users.id WHERE posts.id = ${+req.body.id}`)
 			const subcategory = answer.rows[0]['subcategory']
 			answer.rows[0]['additional_fields'] = null
-			try {
-				const subs = (await pool.query(`SELECT * FROM "subcategories".${subcategory} WHERE post_id = '${req.body.id}'`)).rows
-				let additional_fields = subs[0]
-				if (additional_fields !== undefined) {
-					answer.rows[0]['additional_fields'] = additional_fields
+			console.log(subcategory);
+			if (subcategory !== null) {
+				for (let symbol of subcategory) {
+					if (["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"].includes(symbol) === false) {
+						return("err")
+					}
 				}
-			}
-			catch (e) {
-				console.error(`Внутренняя ошибка api getPost ${e}`)
+				try {
+					const subs = (await pool.query(`SELECT * FROM "subcategories".${subcategory} WHERE post_id = '${req.body.id}'`)).rows
+					let additional_fields = subs[0]
+					if (additional_fields !== undefined) {
+						answer.rows[0]['additional_fields'] = additional_fields
+					}
+				}
+				catch (e) {
+					console.error(`Внутренняя ошибка api getPost ${e}`)
+				}
 			}
 			return(answer.rows[0])
 		}
