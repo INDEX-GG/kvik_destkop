e7a6a2d1512ec9b67285300a1f8d1ed9c3a4203d
Sergey Zinyaev <zinyaevsi@gmail.com>
2022-01-11 10:39:45 +0500

    11.01.21

--- pages/api/getPostsPortion.js
+++ pages/api/getPostsPortion.js
@@ -8,48 +8,35 @@ export default async function handler(req, res) {
         const main = async () => {
             const page_limit = req.body.page_limit
             const page = (req.body.page - 1) * page_limit
-            // const user_id = (req.body.user_id).toString()
             const sort = req.body.sort.toLowerCase()
             const region_includes = req.body.region_includes.toLowerCase()
             let region_excludes = req.body.region_excludes.toLowerCase()
             if (region_excludes === '') {
                 region_excludes = '!'
             }
+            if (typeof req.body.page !== 'number' || typeof req.body.page_limit !== 'number') {
+                return("err")
+            }
             let sort_value
             switch (sort) {
                 case 'default':
-                    sort_value = 'ORDER BY id desc'
+                    sort_value = 'ORDER BY id DESC'
                     break;
                 case 'new':
-                    sort_value = 'ORDER BY id desc'
+                    sort_value = 'ORDER BY id DESC'
                     break;
                 case 'price_by_ascending':
-                    sort_value = 'ORDER BY price asc'
+                    sort_value = 'ORDER BY price ASC'
                     break;
                 case 'price_by_descending':
-                    sort_value = 'ORDER BY price desc'
+                    sort_value = 'ORDER BY price DESC'
                     break;
                 default:
-                    sort_value = ''
+                    sort_value = 'ORDER BY id DESC'
                     break;
             }
-            const answer  = await pool.query(`SELECT users.name AS user_name, users."userPhoto" AS user_photo, users.phone AS user_phone, users.raiting AS user_raiting, posts.id, posts.user_id, posts.category_id, posts.price, posts.old_price, posts.photo, posts.rating, posts.created_at, posts.delivery, posts.reviewed, posts.address, posts.phone, posts.trade, posts.verify_moderator, posts.commercial, posts.secure_transaction, posts.title, posts.email, posts.viewing, posts.city FROM "posts" INNER JOIN "users" ON posts.user_id = users.id WHERE active = 0 AND verify = 0  AND LOWER (city) LIKE '${region_includes}%' AND LOWER (city) NOT LIKE '${region_excludes}%' ${sort_value} LIMIT ${page_limit} offset ${page}`)
-
-
-            // const answer  = await pool.query(`SELECT users.name AS user_name, users."userPhoto" AS user_photo, users.phone AS user_phone, users.raiting AS user_raiting, posts.id, posts.user_id, posts.category_id, posts.price, posts.old_price, posts.photo, posts.rating, posts.created_at, posts.delivery, posts.reviewed, posts.address, posts.phone, posts.trade, posts.verify_moderator, posts.commercial, posts.secure_transaction, posts.title, posts.email, posts.viewing FROM "posts" INNER JOIN "users" ON posts.user_id = users.id WHERE active = 0 AND verify = 0 ORDER BY id desc LIMIT ${page_limit} offset ${page}`)
-            // for (let index in answer.rows) {
-            //     let views = (answer.rows[index]).viewing
-            //     let preList = views.substring(1);
-            //     let preList2 = preList.substring(0, preList.length - 1);
-            //     let list = preList2.split(',');
-            //     if (list.includes(user_id)) {
-            //         (answer.rows[index]).viewing_bool = true
-            //     } else {
-            //         (answer.rows[index]).viewing_bool = false
-            //     }
-            // }
-
-
+            //// const answer  = await pool.query(`SELECT users.name AS user_name, users."userPhoto" AS user_photo, users.phone AS user_phone, users.raiting AS user_raiting, posts.id, posts.user_id, posts.category_id, posts.price, posts.old_price, posts.photo, posts.rating, posts.created_at, posts.delivery, posts.reviewed, posts.address, posts.phone, posts.trade, posts.verify_moderator, posts.commercial, posts.secure_transaction, posts.title, posts.email, posts.viewing, posts.city FROM "posts" INNER JOIN "users" ON posts.user_id = users.id WHERE active = 0 AND verify = 0  AND LOWER (city) LIKE '${region_includes}%' AND LOWER (city) NOT LIKE '${region_excludes}%' ${sort_value} LIMIT ${page_limit} offset ${page}`)
+            const answer  = await pool.query(`SELECT users.name AS user_name, users."userPhoto" AS user_photo, users.phone AS user_phone, users.raiting AS user_raiting, posts.id, posts.user_id, posts.category_id, posts.price, posts.old_price, posts.photo, posts.rating, posts.created_at, posts.delivery, posts.reviewed, posts.address, posts.phone, posts.trade, posts.verify_moderator, posts.commercial, posts.secure_transaction, posts.title, posts.email, posts.viewing, posts.city FROM "posts" INNER JOIN "users" ON posts.user_id = users.id WHERE active = 0 AND verify = 0  AND LOWER (city) LIKE $1 AND LOWER (city) NOT LIKE $2 ${sort_value} LIMIT $3 offset $4;`, [region_includes + '%', region_excludes + '%', page_limit, page])
             return(answer.rows)
         }
         try {
