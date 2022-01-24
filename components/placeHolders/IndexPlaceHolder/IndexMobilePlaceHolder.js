a54100078eb2d0b8f702ed9444f0bf17793731a1
satonin12 <vlad.satonin74@gmail.com>
2022-01-24 12:03:31 +0500

    add moble placeholder in index page

---
+++ components/placeHolders/IndexPlaceHolder/IndexMobilePlaceHolder.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+import {Grid, Skeleton} from "@mui/material";
+import { Box, makeStyles } from '@material-ui/core'
+
+import SkeletonFirstLine from "./SkeletonFirstLine";
+
+const useStyles = makeStyles(() => ({
+  skeletonSearch: {
+    display: "flex",
+    gap: '10px',
+    flexDirection: "column"
+  }
+}))
+
+const IndexMobilePlaceHolder = () => {
+  const classes = useStyles();
+
+  return (
+    <Box sx={{ width: '100%' }}>
+    <Grid container rowSpacing={1} columnSpacing={{ md: 6 }} sx={{padding: '0 10px'}}>
+
+        {/* первая линия */}
+        <Grid item sx={{width: '100%', marginTop: '12px'}}>
+            <Box
+              className={classes.skeletonSearch}
+            >
+                <Box>
+                    <Skeleton animation="wave" variant="rectangular" width="auto" height="71px" sx={{ bgcolor: '#F2F3F4', borderRadius: '8px', margin: 'auto' }}/>
+                </Box>
+                <Grid
+                  container
+                  flexDirection='row'
+                  justifyContent='space-between'
+
+                >
+                    <Skeleton animation="wave" variant="rectangular" width="101px" height="20px" sx={{ bgcolor: '#F2F3F4' , borderRadius: '5px'  }} />
+                    <Skeleton animation="wave" variant="rectangular" width="64px" height="20px" sx={{ bgcolor: '#F2F3F4' , borderRadius: '5px'  }} />
+                </Grid>
+            </Box>
+        </Grid>
+
+        {/* таблички обьявлений */}
+        <Grid item sx={{width: '100%', height: '100%', marginTop: '20px'}}>
+          <Grid
+            container
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            {[1,2,3,4].map((_, i) => (
+              <Skeleton key={i} animation="wave" variant="rectangular" width="164px" height="240px" sx={{ bgcolor: '#F2F3F4', borderRadius: '8px', marginBottom: '12px' }} />
+            ))}
+          </Grid>
+        </Grid>
+
+
+    </Grid>
+</Box>
+  )
+}
+
+export default IndexMobilePlaceHolder;
--- components/placeHolders/IndexPlaceHolder/IndexPlaceHolder.js
+++ components/placeHolders/IndexPlaceHolder/IndexPlaceHolder.js
@@ -4,6 +4,9 @@ import {Box, makeStyles} from "@material-ui/core";
 import SkeletonFirstLine from "./SkeletonFirstLine";
 import SkeletonSecondLine from "./SkeletonSecondLine";
 
+import {useMedia} from "../../../hooks/useMedia";
+import IndexMobilePlaceHolder from './IndexMobilePlaceHolder';
+
 const useStyles = makeStyles(() => ({
     skeletonSearch: {
         display: "flex",
@@ -14,59 +17,68 @@ const useStyles = makeStyles(() => ({
 
 const IndexPlaceHolder = () => {
     const classes = useStyles();
+    const {matchesMobile, matchesTablet} = useMedia();
+
     return (
-        <Box sx={{ width: '100%' }}>
-            <Grid container rowSpacing={1} columnSpacing={{ md: 6 }}>
+        <>
+            {!matchesMobile
+            ? (
+            <Box sx={{ width: '100%' }}>
+                <Grid container rowSpacing={1} columnSpacing={{ md: 6 }}>
 
-                <Grid item xs={6} md={12}>
-                    <Box className={classes.skeletonSearch}>
-                        <Box>
-                            <Skeleton animation="wave" variant="rectangular" width="15%" sx={{ bgcolor: '#F2F3F4', borderRadius: '15px' }}/>
-                        </Box>
-                        <Box>
-                            <Skeleton  animation="wave" variant="rectangular"  sx={{ bgcolor: '#F2F3F4' , borderRadius: '15px'  }}><div style={{ width: "1248px", height: "182px" }} />
-                            </Skeleton>
-                        </Box>
-                    </Box>
-                </Grid>
-
-                {/*первая линия обьявлений*/}
-                <Grid item md={12}>
-                    <Grid item xs={8}>
-                        <Box>
-                            <Skeleton  animation="wave" variant="rectangular"  width="15%"  sx={{ bgcolor: '#F2F3F4', borderRadius: '15px'  }}><div style={{ paddingTop: '13%'}} />
-                            </Skeleton>
+                    <Grid item xs={6} md={12}>
+                        <Box className={classes.skeletonSearch}>
+                            <Box>
+                                <Skeleton animation="wave" variant="rectangular" width="15%" sx={{ bgcolor: '#F2F3F4', borderRadius: '15px' }}/>
+                            </Box>
+                            <Box>
+                                <Skeleton  animation="wave" variant="rectangular"  sx={{ bgcolor: '#F2F3F4' , borderRadius: '15px'  }}><div style={{ width: "1248px", height: "182px" }} />
+                                </Skeleton>
+                            </Box>
                         </Box>
                     </Grid>
-                    <Grid item container md={12} style={{paddingTop: "10px"}}  direction="row">
-                        <SkeletonFirstLine/>
-                        <SkeletonFirstLine/>
-                        <SkeletonFirstLine/>
-                        <SkeletonFirstLine/>
-                    </Grid>
-                </Grid>
 
-                {/*вторая линия обьявлений*/}
-                <Grid item md={12}>
-                    <Grid item container md={12} style={{paddingTop: "10px"}}  direction="row">
-                        <SkeletonSecondLine/>
-                        <SkeletonFirstLine/>
-                        <SkeletonFirstLine/>
+                    {/*первая линия обьявлений*/}
+                    <Grid item md={12}>
+                        <Grid item xs={8}>
+                            <Box>
+                                <Skeleton  animation="wave" variant="rectangular"  width="15%"  sx={{ bgcolor: '#F2F3F4', borderRadius: '15px'  }}><div style={{ paddingTop: '13%'}} />
+                                </Skeleton>
+                            </Box>
+                        </Grid>
+                        <Grid item container md={12} style={{paddingTop: "10px"}}  direction="row">
+                            <SkeletonFirstLine/>
+                            <SkeletonFirstLine/>
+                            <SkeletonFirstLine/>
+                            <SkeletonFirstLine/>
+                        </Grid>
                     </Grid>
-                </Grid>
 
-                {/*третья линия обьявлений*/}
-                <Grid item md={12}>
-                    <Grid item container md={12} style={{paddingTop: "10px"}}  direction="row">
-                        <SkeletonFirstLine/>
-                        <SkeletonFirstLine/>
-                        <SkeletonFirstLine/>
-                        <SkeletonFirstLine/>
+                    {/*вторая линия обьявлений*/}
+                    <Grid item md={12}>
+                        <Grid item container md={12} style={{paddingTop: "10px"}}  direction="row">
+                            <SkeletonSecondLine/>
+                            <SkeletonFirstLine/>
+                            <SkeletonFirstLine/>
+                        </Grid>
                     </Grid>
-                </Grid>
 
-            </Grid>
-        </Box>
+                    {/*третья линия обьявлений*/}
+                    <Grid item md={12}>
+                        <Grid item container md={12} style={{paddingTop: "10px"}}  direction="row">
+                            <SkeletonFirstLine/>
+                            <SkeletonFirstLine/>
+                            <SkeletonFirstLine/>
+                            <SkeletonFirstLine/>
+                        </Grid>
+                    </Grid>
+
+                </Grid>
+            </Box>
+            ) : (
+                <IndexMobilePlaceHolder />
+            )}
+        </>
     );
 };
 
