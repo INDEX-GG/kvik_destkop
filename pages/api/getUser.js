0ed01dcf6a02b91def90a27798ac1ad910c190db
Sergey Zinyaev <zinyaevsi@gmail.com>
2022-01-25 16:20:28 +0500

    25.01.21

--- pages/api/getUser.js
+++ pages/api/getUser.js
@@ -11,31 +11,17 @@ export default async function handler(req, res) {
 
 
 			//SWIPESWIPESWIPE//SWIPESWIPESWIPE//SWIPESWIPESWIPE
-			//
+
 			// let user = await pool.query(`SELECT users."name", users."userPhoto", users."about", users."createdAt", users."phone", users."email", users."raiting", users."location", users."address" FROM "public"."users" WHERE users."id" = $1`, [user_id])
 			// let user_obj = user.rows[0]
+			//
 			// let favorites = await pool.query(`SELECT array_agg(liked_post_id) as liked_post_id FROM "public"."favorites" WHERE user_id = $1`, [user_id])
 			// let subscriptions = await pool.query(`SELECT COUNT(subscription) FROM "public"."subscriptions" WHERE user_id = $1`, [user_id])
 			// let subscribers = await pool.query(`SELECT COUNT(user_id) FROM "public"."subscriptions" WHERE subscription = $1`, [user_id])
 			//
-			//
-			//
-			//
-			// let active_posts  = await pool.query(`SELECT COUNT(id) FROM "posts" WHERE active = 0 AND verify = 0  AND posts.user_id = $1 AND ((active_time >= $2) OR (active_time IS NULL))`, [user_id, new Date()])
-			// let archive_posts = await pool.query(`SELECT COUNT(id) FROM "posts" WHERE active != 0 AND verify = 0  AND posts.user_id = $1 AND ((active_time >= $2) OR (active_time IS NULL))`, [user_id, new Date()])
-			// let wait_posts = await pool.query(`SELECT COUNT(id) FROM "posts" WHERE verify != 0  AND posts.user_id = $1 AND ((active_time >= $2) OR (active_time IS NULL))`, [user_id, new Date()])
-			//
-			// user_obj.search_count = 0
 			// user_obj.favorites = favorites.rows[0].liked_post_id
-			// user_obj.favorites_count = favorites.rows[0].liked_post_id.length
 			// user_obj.subscriptions_count = parseInt(subscriptions.rows[0].count)
 			// user_obj.subscribers_count = parseInt(subscribers.rows[0].count)
-			// user_obj.active_posts_count = parseInt(active_posts.rows[0].count)
-			// user_obj.wait_posts_count = parseInt(wait_posts.rows[0].count)
-			// user_obj.archive_posts_count = parseInt(archive_posts.rows[0].count)
-
-
-
 
 
 
@@ -65,7 +51,6 @@ export default async function handler(req, res) {
 		finally {
 			await pool.end()
 		}
-
 	} else {
 		res.json({ message: 'method not allowed' })
 		res.status(405).end()
