a126b55688249d8af321b68a93ee09ee18ffe679
Sergey Zinyaev <zinyaevsi@gmail.com>
2022-01-26 14:37:43 +0500

    26.01.21

--- pages/api/getUser.js
+++ pages/api/getUser.js
@@ -7,27 +7,30 @@ export default async function handler(req, res) {
 		const main = async () => {
 			const user_id = req.body.id
 
-
-
+			// const jwt = require("jsonwebtoken");
+			// const token = req.headers["x-access-token"];
+			// if (!token) {
+			// 	return res.status(403).send("A token is required for authentication");
+			// }
+			// try {
+			// 	jwt.verify(token, process.env.NEXT_PUBLIC_JWT_SECRET);
+			// } catch (err) {
+			// 	return res.status(401).send("Invalid Token");
+			// }
+			// const tokenUser = jwt.verify(token, process.env.NEXT_PUBLIC_JWT_SECRET).sub
+			// if (parseInt(req.body.id, 10) !== tokenUser) {
+			// 	return res.status(403).send("Invalid Token");
+			// }
 
 			//SWIPESWIPESWIPE//SWIPESWIPESWIPE//SWIPESWIPESWIPE
 
-			// let user = await pool.query(`SELECT users."name", users."userPhoto", users."about", users."createdAt", users."phone", users."email", users."raiting", users."location", users."address" FROM "public"."users" WHERE users."id" = $1`, [user_id])
-			// let user_obj = user.rows[0]
-			//
-			// let favorites = await pool.query(`SELECT array_agg(liked_post_id) as liked_post_id FROM "public"."favorites" WHERE user_id = $1`, [user_id])
-			// let subscriptions = await pool.query(`SELECT COUNT(subscription) FROM "public"."subscriptions" WHERE user_id = $1`, [user_id])
-			// let subscribers = await pool.query(`SELECT COUNT(user_id) FROM "public"."subscriptions" WHERE subscription = $1`, [user_id])
-			//
-			// user_obj.favorites = favorites.rows[0].liked_post_id
-			// user_obj.subscriptions_count = parseInt(subscriptions.rows[0].count)
-			// user_obj.subscribers_count = parseInt(subscribers.rows[0].count)
-
-
-
+			// let user = await pool.query(`SELECT users."name", users."userPhoto", users."about", users."createdAt", users."phone", users."email", users."raiting", users."location", users."address",
+			// 	(SELECT COUNT(subscription) FROM "public"."subscriptions" WHERE user_id = $1) AS "subscriptions_count",
+			// 	(SELECT COUNT(user_id) FROM "public"."subscriptions" WHERE subscription = $1) AS "subscribers_count",
+			// 	array(SELECT array_agg(liked_post_id) as liked_post_id FROM "public"."favorites" WHERE user_id = $1)
+			// 	FROM "public"."users" WHERE users."id" = $1`, [user_id])
 
 			let user = await pool.query(`SELECT users."favorites", users."subscriptions", users."subscribers", users."name", users."userPhoto", users."about", users."createdAt", users."phone", users."email", users."raiting", users."location", users."address" FROM "public"."users" WHERE users."id" = $1`, [user_id])
-			let user_obj = user.rows[0]
 
 			//SWIPESWIPESWIPE//SWIPESWIPESWIPE//SWIPESWIPESWIPE
 
@@ -35,7 +38,7 @@ export default async function handler(req, res) {
 
 
 
-			return user_obj
+			return user.rows[0]
 		}
 		try {
 			let response = await main();
