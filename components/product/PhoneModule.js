117cde54e8bce5867f2bdb8795b3e76be9924b64
karimovmaxdev <karimov.maxdev@gmail.com>
2022-02-01 17:16:31 +0500

    phone fixed

--- components/product/PhoneModule.js
+++ components/product/PhoneModule.js
@@ -8,79 +8,84 @@ import {useRouter} from "next/router";
 import ProductNumberPng from "./ProductNumberPng";
 // import {useProduct} from "../../hooks/useProduct";
 import {STATIC_URL} from "../../lib/constants";
+import CryptoJS from "crypto-js";
 
 
-  const useStyles = makeStyles(() => ({
-    modalNumber: {
-      fontSize: "12px",
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      padding: "16px 8px 0px",
-    },
-    userProfile: {
-      display: "flex",
-      alignItems: "center",
-      marginBottom: "20px",
-      cursor: "pointer",
-    },
-    userInf: {
-      marginLeft: "14px",
-    },
-    userName: {
-      fontSize: "12px",
-      color: "#2C2C2C",
-      "&:hover": {
-        transition: "all 150ms ease-in-out",
-        textDecoration: "underline",
-      }
-    },
-    userStar: {
-      display: "flex",
-      alignItems: "center",
-    },
-    numberStar: {
-      marginRight: "4px",
-      color: "#5A5A5A",
-      fontSize: "14px",
-      fontWeight: "500",
-    },
-    userPhone: {
-      color: "#2C2C2C",
-      fontSize: "30px",
-      marginBottom: "7px",
-    },
-    userMessage: {
-      color: "#5A5A5A",
-      marginBottom: "30px",
-      textAlign: "center",
-      visibility: "hidden",
-    },
-    warningMessage: {
-      marginBottom: "15px",
-      fontWeight: "500",
-      color: "#5A5A5A",
-    },
-    warningBlock: {
-      color: "#5A5A5A",
-      fontWeight: "500",
-    },
-    warningItem: {
-      marginBottom: "20px",
-    },
-    small: {
-      width: "56px",
-      height: "56px",
-      
-    },
-  }));
+const useStyles = makeStyles(() => ({
+  modalNumber: {
+    fontSize: "12px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "16px 8px 0px",
+  },
+  userProfile: {
+    display: "flex",
+    alignItems: "center",
+    marginBottom: "20px",
+    cursor: "pointer",
+  },
+  userInf: {
+    marginLeft: "14px",
+  },
+  userName: {
+    fontSize: "12px",
+    color: "#2C2C2C",
+    "&:hover": {
+      transition: "all 150ms ease-in-out",
+      textDecoration: "underline",
+    }
+  },
+  userStar: {
+    display: "flex",
+    alignItems: "center",
+  },
+  numberStar: {
+    marginRight: "4px",
+    color: "#5A5A5A",
+    fontSize: "14px",
+    fontWeight: "500",
+  },
+  userPhone: {
+    color: "#2C2C2C",
+    fontSize: "30px",
+    marginBottom: "7px",
+  },
+  userMessage: {
+    color: "#5A5A5A",
+    marginBottom: "30px",
+    textAlign: "center",
+    visibility: "hidden",
+  },
+  warningMessage: {
+    marginBottom: "15px",
+    fontWeight: "500",
+    color: "#5A5A5A",
+  },
+  warningBlock: {
+    color: "#5A5A5A",
+    fontWeight: "500",
+  },
+  warningItem: {
+    marginBottom: "20px",
+  },
+  small: {
+    width: "56px",
+    height: "56px",
+    
+  },
+}));
 
+function decrypt(string) {
+  return CryptoJS.AES.decrypt(string, process.env.NEXT_PUBLIC_MY_SECRET).toString(CryptoJS.enc.Utf8);
+}
 
-export default function PhoneModule({dialog, setDialog, userPhotoInIndex, userPhone, userName, userRating, productInfo}) {
+export default function PhoneModule({dialog, setDialog, userPhotoInIndex, /*userPhone,*/ userName, userRating, productInfo}) {
   const classes = useStyles();
   const router = useRouter()
-  const { name, userPhoto, raiting, user_id, isLoading } = productInfo
+  const { name, userPhoto, raiting, user_id, isLoading, user_phone } = productInfo
   const {sellerPhone} = useOutherUser(user_id)
+  const decrpytedPhone = decrypt(user_phone)
 
 
   return (
@@ -101,7 +106,7 @@ export default function PhoneModule({dialog, setDialog, userPhotoInIndex, userPh
           </div>
         </div>
          {/*<h2 className={classes.userPhone}>{sellerPhone || userPhone || "Телефон не указан"}</h2>*/}
-        { sellerPhone ? <ProductNumberPng name={sellerPhone ? sellerPhone : userPhone} x={0} y={25}/> : null}
+        { sellerPhone ? <ProductNumberPng name={decrpytedPhone ? decrpytedPhone : sellerPhone} x={0} y={25}/> : null}
         <p className={classes.userMessage}>Номер защищён: смс и сообщения в Viber, WhatsApp и других мессенджерах не будут доставлены</p>
         <div className={classes.warningMessage}>Советы о том как не попасться мошенникам</div>
         <ul className={classes.warningBlock}>
