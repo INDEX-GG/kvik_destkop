2d44a5e5b07caf78d7499fbadf5e49fdaf8723ad
satonin12 <vlad.satonin74@gmail.com>
2022-02-01 11:11:36 +0500

    hit a non-existent product

--- hooks/useProduct.js
+++ hooks/useProduct.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { STATIC_URL } from '../lib/constants';
 import { useAuth } from '../lib/Context/AuthCTX';
 import { getDataByPost } from '../lib/fetch';
+import router from 'next/router';
 
 
 export function useProduct(id) {
@@ -14,11 +15,12 @@ export function useProduct(id) {
 		if (typeof id === 'string' || typeof id === 'number') {
 			// запрос для старой версии бека (id это число )
 			// getDataByPost('/api/getPost?123', { id: id, 'user_id': userId })
+			console.log('отправляем запрос: ')
 			getDataByPost('/api/getPost?123', { id: intId, 'user_id': userId })
 				.then((r) => {
 
 					if (r !== undefined) {
-						// console.log('rrrrrrrrrr',r)
+						console.log('rrrrrrrrrr',r)
 
 						let photoes = JSON.parse(r.photo);
 						r.chatProductPhoto = photoes.photos[0]
@@ -31,6 +33,14 @@ export function useProduct(id) {
 						r.chatPhoto = r.userPhoto;
 						r.userPhoto = `${STATIC_URL}/${r.userPhoto}`;
 						setProductInfo(r);
+					}
+				})
+				.catch((e) => {
+					const resError = e.response
+					if(resError.data?.message === 404) {
+						// TODO: перербрасывать на страницу ошибка сервера
+						// router.push("/500")
+						console.log(1)
 					}
 				})
 		}
