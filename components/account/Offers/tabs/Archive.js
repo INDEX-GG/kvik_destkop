eb739ea4e8203ffbf0f4341a36396f5a441ed258
EvERI88 <volktyt202@gmail.com>
2022-02-10 11:12:43 +0500

    build fix

--- components/account/Offers/tabs/Archive.js
+++ components/account/Offers/tabs/Archive.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import EmptyPlaceholder from "../../../EmptyPlaceholder";
 import OfferModal from "../../../OfferModal";
-import OfferArchive from "../card/offerArchive";
 import { useOfferAccount } from "../../../../lib/Context/OfferAccountCTX";
 import { Checkbox, makeStyles, Dialog  } from "@material-ui/core";
 import FiberManualRecordOutlinedIcon from '@material-ui/icons/FiberManualRecordOutlined';
