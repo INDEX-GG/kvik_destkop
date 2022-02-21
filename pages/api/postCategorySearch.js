bcfbb00ccefd6c0b1ed746a0b5faf22ff298a799
Sergey Zinyaev <zinyaevsi@gmail.com>
2022-02-21 15:40:48 +0500

    21.02

--- pages/api/postCategorySearch.js
+++ pages/api/postCategorySearch.js
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
 		const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 		const main = async () => {
+			let date = new Date()
 			const data = req.body.data.toLowerCase();
 			const page_limit = req.body.page_limit
 			const page = (req.body.page - 1) * page_limit
@@ -35,7 +36,16 @@ export default async function handler(req, res) {
 			if (region_excludes === '') {
 				region_excludes = '!'
 			}
-			const answer  = await pool.query(`SELECT users.name AS user_name, users."userPhoto" AS user_photo, users.phone AS user_phone, users.raiting AS user_raiting, posts.id, posts.user_id, posts.category_id, posts.price, posts.old_price, posts.photo, posts.rating, posts.created_at, posts.delivery, posts.reviewed, posts.address, posts.phone, posts.trade, posts.verify_moderator, posts.commercial, posts.secure_transaction, posts.title, posts.email, posts.viewing FROM "posts" INNER JOIN "users" ON posts.user_id = users.id WHERE LOWER (category_id) LIKE $1 AND active = 0 AND verify = 0 AND LOWER (city) LIKE $2 AND LOWER (city) NOT LIKE $3 AND ((active_time >= $4) OR (active_time IS NULL)) ${sort_value} LIMIT $5 offset $6`, [data + '%', region_includes + '%', region_excludes + '%', new Date(), page_limit, page])
+			const answer  = await pool.query(`SELECT users.name AS user_name, users."userPhoto" AS user_photo, users.phone AS user_phone, users.raiting AS user_raiting, posts.id, posts.user_id, posts.category_id, posts.price, posts.old_price, posts.photo, posts.rating, posts.created_at, posts.delivery, posts.reviewed, posts.address, posts.phone, posts.trade, posts.verify_moderator, posts.commercial, posts.secure_transaction, posts.title, posts.email, posts.viewing, posts.color_selection, posts.size_selection FROM "posts" INNER JOIN "users" ON posts.user_id = users.id WHERE LOWER (category_id) LIKE $1 AND active = 0 AND verify = 0 AND LOWER (city) LIKE $2 AND LOWER (city) NOT LIKE $3 AND ((active_time >= $4) OR (active_time IS NULL)) ${sort_value} LIMIT $5 offset $6`, [data + '%', region_includes + '%', region_excludes + '%', new Date(), page_limit, page])
+
+			answer.rows.forEach(
+				element => {
+					element.highlighting = element.color_selection >= date;
+					element.selection_size = element.size_selection >= date;
+					delete element.color_selection
+					delete element.size_selection
+				});
+
 			return(answer.rows)
 		}
 		try {
--- pages/api/searchInsideCategory.js
+++ pages/api/searchInsideCategory.js
@@ -31,13 +31,23 @@ export default async function handler(req, res) {
 					sort_value = ''
 					break;
 			}
+			let date = new Date()
 			const region_includes = req.body.region_includes.toLowerCase()
 			let region_excludes = req.body.region_excludes.toLowerCase()
 			if (region_excludes === '') {
 				region_excludes = '!'
 			}
-			const answer =  await pool.query(`SELECT * FROM posts WHERE ((active_time >= $1) OR (active_time IS NULL)) AND LOWER (category_id) LIKE $2 AND active = 0 AND verify = 0 AND (LOWER (title) LIKE $3 OR LOWER (description) LIKE $3) AND LOWER (city) LIKE $4 AND LOWER (city) NOT LIKE $5 ${sort_value} LIMIT $6 offset $7`, [new Date() ,category + '%', '%' + text + '%', region_includes + '%', region_excludes + '%', page_limit, page])
-		    return (answer.rows)
+			const answer =  await pool.query(`SELECT users.name AS user_name, users."userPhoto" AS user_photo, users.phone AS user_phone, users.raiting AS user_raiting, posts.id, posts.user_id, posts.category_id, posts.price, posts.old_price, posts.photo, posts.rating, posts.created_at, posts.delivery, posts.reviewed, posts.address, posts.phone, posts.trade, posts.verify_moderator, posts.commercial, posts.secure_transaction, posts.title, posts.email, posts.viewing, posts.color_selection, posts.size_selection FROM "posts" INNER JOIN "users" ON posts.user_id = users.id WHERE ((active_time >= $1) OR (active_time IS NULL)) AND LOWER (category_id) LIKE $2 AND active = 0 AND verify = 0 AND (LOWER (title) LIKE $3 OR LOWER (description) LIKE $3) AND LOWER (city) LIKE $4 AND LOWER (city) NOT LIKE $5 ${sort_value} LIMIT $6 offset $7`, [new Date() ,category + '%', '%' + text + '%', region_includes + '%', region_excludes + '%', page_limit, page])
+
+			answer.rows.forEach(
+				element => {
+					element.highlighting = element.color_selection >= date;
+					element.selection_size = element.size_selection >= date;
+					delete element.color_selection
+					delete element.size_selection
+				});
+
+			return (answer.rows)
         }
 		try {
 			let response = await main();
