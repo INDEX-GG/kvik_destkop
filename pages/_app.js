b2e84f92775ade4dcf04e7dc7c04ae531a18ce92
Rodion Pyatigroskiy <81239058+NenadoPL1Z@users.noreply.github.com>
2022-03-09 16:46:00 +0500

    FIX-YMETRICKA

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import Router from "next/router";
-import withYM from "next-ym";
 import { DefaultSeo } from 'next-seo';
 import { SWRConfig } from 'swr'
 import 'swiper/swiper.scss';
@@ -18,45 +16,43 @@ import CityProvider from '../lib/Context/CityCTX';
 import StoreProvider from '../lib/Context/Store';
 import StatisticsProvider from '../lib/Context/StatisticsCTX';
 import LoginDrawerProvider from '../lib/Context/DialogCTX';
-// import YandexMetrika from "../src/components/AnyPage/YandexMetrika/YandexMetrika";
-import {YANDEX_METRIK} from '../lib/constants';
+import YandexMetrika from "../src/components/AnyPage/YandexMetrika/YandexMetrika";
 
-export default withYM(YANDEX_METRIK, Router)(
-	function MyApp({ Component, pageProps }) {
-		return (
-			<ErrorBoundary>
-				<DefaultSeo {...defaultSEO}/>
-				<SWRConfig
-					value={{
-						fetcher: fetch,
-						onError: (err) => {
-							console.error(err)
-						},
-					}}
-				>
-					<Head>
-						<meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-					</Head>
-					{/*<YandexMetrika>*/}
-						<AuthProvider>
-							<ThemeProvider theme={theme}>
-								<StatisticsProvider>
-									<StoreProvider>
-										<CityProvider>
-											<LoginDrawerProvider>
-												<MainLayout>
-													<CssBaseline />
-													<Component {...pageProps} />
-												</MainLayout>
-											</LoginDrawerProvider>
-										</CityProvider>
-									</StoreProvider>
-								</StatisticsProvider>
-							</ThemeProvider>
-						</AuthProvider>
-					{/*</YandexMetrika>*/}
-				</SWRConfig>
-			</ErrorBoundary>
-		)
-	}
-);
+
+export default function MyApp({ Component, pageProps }) {
+	return (
+		<ErrorBoundary>
+			<DefaultSeo {...defaultSEO}/>
+			<SWRConfig
+				value={{
+					fetcher: fetch,
+					onError: (err) => {
+						console.error(err)
+					},
+				}}
+			>
+				<Head>
+					<meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+				</Head>
+				<YandexMetrika>
+				<AuthProvider>
+					<ThemeProvider theme={theme}>
+						<StatisticsProvider>
+							<StoreProvider>
+								<CityProvider>
+									<LoginDrawerProvider>
+										<MainLayout>
+											<CssBaseline />
+											<Component {...pageProps} />
+										</MainLayout>
+									</LoginDrawerProvider>
+								</CityProvider>
+							</StoreProvider>
+						</StatisticsProvider>
+					</ThemeProvider>
+				</AuthProvider>
+				</YandexMetrika>
+			</SWRConfig>
+		</ErrorBoundary>
+	)
+}
