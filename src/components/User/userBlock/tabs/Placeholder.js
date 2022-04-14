8877227cf418d18eb3b6a4c95c939e371c2d9a80
Evgeny_Kiselev <mor-cre@mail.ru>
2022-04-14 16:51:39 +0500

    начало работы над PLaceholder

--- src/components/User/userBlock/tabs/Placeholder.js
+++ src/components/User/userBlock/tabs/Placeholder.js
@@ -1,34 +1,45 @@
 import React from "react";
-
-function Placeholder({user}) {
+import { Box } from "@material-ui/core";
+const Placeholder = ({ user }) => {
   return (
-    <div className="clientPage__container_bottom">
-      <div className="clientPage__container_content">
-        <div className="phOffers">
-          {user ? user == 1 ? 
-		  <div className="phTitle dark">У этого пользователя ещё нет объявлений</div> : 
-		  <div className="phTitle dark">У этого пользователя нет завершенных объявлений</div> :
-		  <div className="phTitle dark">У этого пользователя ещё нет объявлений</div>}
-          <div className="phContent">
-            <div className="phCard">
-              <div className="phCardPhoto"></div>
-              <div className="phCardTitle"></div>
-              <div className="phCardSubtitle"></div>
-            </div>
-            <div className="phCard">
-              <div className="phCardPhoto"></div>
-              <div className="phCardTitle"></div>
-              <div className="phCardSubtitle"></div>
-            </div>
-            <div className="phCard">
-              <div className="phCardPhoto"></div>
-              <div className="phCardTitle"></div>
-              <div className="phCardSubtitle"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Box className="clientPage__container_bottom">
+      <Box className="clientPage__container_content">
+        <Box className="phOffers">
+          {user ? (
+            user == 1 ? (
+              <Box className="phTitle dark">
+                У этого пользователя ещё нет объявлений
+              </Box>
+            ) : (
+              <Box className="phTitle dark">
+                У этого пользователя нет завершенных объявлений
+              </Box>
+            )
+          ) : (
+            <Box className="phTitle dark">
+              У этого пользователя ещё нет объявлений
+            </Box>
+          )}
+          <Box className="phContent">
+            <Box className="phCard">
+              <Box className="phCardPhoto" />
+              <Box className="phCardTitle" />
+              <Box className="phCardSubtitle" />
+            </Box>
+            <Box className="phCard">
+              <Box className="phCardPhoto" />
+              <Box className="phCardTitle" />
+              <Box className="phCardSubtitle" />
+            </Box>
+            <Box className="phCard">
+              <Box className="phCardPhoto" />
+              <Box className="phCardTitle" />
+              <Box className="phCardSubtitle" />
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
-}
-export default Placeholder;
+};
+export default React.memo(Placeholder);
