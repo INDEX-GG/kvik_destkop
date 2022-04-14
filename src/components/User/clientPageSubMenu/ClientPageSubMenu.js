93597852182f8e4db23fc109c3b20ae3bfcd744a
Evgeny_Kiselev <mor-cre@mail.ru>
2022-04-14 09:15:53 +0500

    сделал ClassNames в ClientPAgeSubMenu

--- src/components/User/clientPageSubMenu/ClientPageSubMenu.js
+++ src/components/User/clientPageSubMenu/ClientPageSubMenu.js
@@ -4,17 +4,18 @@ import { useStore } from "#lib/Context/Store";
 import { useStatistics } from "#lib/Context/StatisticsCTX";
 import { useRouter } from "next/router";
 import { Box } from "@material-ui/core";
+import { useClientSubMenu } from "./style";
 
 const ClientPageSubMenu = () => {
   const [loading /* setLoading*/] = useState(false);
   const { id /*token*/ } = useAuth();
-
   const { userInfo } = useStore();
   const [userBool, setUserBool] = useState(false);
   const { addSubscribers, addUnsubscribe } = useStatistics();
-
   const router = useRouter();
   const sellerId = parseInt(router.query.id);
+
+  const classes = useClientSubMenu();
 
   const subscribeClickHandler = () => {
     if (!id) return;
@@ -35,7 +36,8 @@ const ClientPageSubMenu = () => {
     <>
       <button
         disabled={loading}
-        className="btnSubscribe"
+        // className="btnSubscribe"
+        className={classes.btnSubscribe}
         // onClick={() => subscribeUser()}
         onClick={() => {
           setUserBool(!userBool);
@@ -44,8 +46,18 @@ const ClientPageSubMenu = () => {
       >
         {userBool ? "Отписаться" : "Подписаться"}
       </button>
-      <Box className="ad__block_bottom__adaptive_right">
-        <a className="SellerInfoComplain small light underline">Пожаловаться</a>
+      <Box
+        // className="ad__block_bottom__adaptive_right"
+        className={classes.ad__block_bottom__adaptive_right}
+      >
+        <a
+          // className="SellerInfoComplain small light underline"
+          className={`${classes.SellerInfoComplain} ${classes.small} ${
+            classes.light
+          } ${classes.underline}`}
+        >
+          Пожаловаться
+        </a>
       </Box>
     </>
   );
