6be33b9e309fc5036df97e96f30f42bd0b7e119d
Evgeny_Kiselev <mor-cre@mail.ru>
2022-04-18 11:05:00 +0500

    удалил modalmobile и использовал готовый компонент

--- src/components/User/userContentMobile/UserContentMobile.js
+++ src/components/User/userContentMobile/UserContentMobile.js
@@ -1,34 +1,22 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
 import UserContent from "../userContent/UserContent";
-import MobileModal from "../../MobileModal/MobileModal";
-
-// import MobileModal from "../../../../components/MobileModal";
+import CustomModalUI from "src/UI/UIcomponent/CustomModal/CustomModalUI";
 
 const UserContentMobile = () => {
   const router = useRouter();
   const [isShowProfileDialog, setIsShowProfileDialog] = useState(true);
   return (
-    <MobileModal
+    <CustomModalUI
       title="Профиль"
-      dialog={isShowProfileDialog || false}
-      close={() => {
+      open={isShowProfileDialog}
+      handleCloseModal={() => {
         router.back();
         setIsShowProfileDialog((prevState) => !prevState);
       }}
     >
       {<UserContent />}
-    </MobileModal>
-    // <MobileModal
-    //   title="Профиль"
-    //   dialog={isShowProfileDialog || false}
-    //   close={() => {
-    //     router.back();
-    //     setIsShowProfileDialog((prevState) => !prevState);
-    //   }}
-    // >
-    //   {<UserContent />}
-    // </MobileModal>
+    </CustomModalUI>
   );
 };
 
