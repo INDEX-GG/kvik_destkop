ce7989804f9063b48281b4b673207d326756e1b7
Evgeny_Kiselev <mor-cre@mail.ru>
2022-04-19 10:09:09 +0500

    отредактировал placeholder

--- src/components/User/userBlock/tabs/Placeholder.js
+++ src/components/User/userBlock/tabs/Placeholder.js
@@ -4,40 +4,42 @@ import { Box } from "@material-ui/core";
 import { usePlayceholderStyle } from "./soldStyle";
 const Placeholder = ({ user }) => {
   const classes = usePlayceholderStyle();
+
+  const userBlock = user ? (
+    user == 1 ? (
+      <Box className="phTitle dark" /* не нашел  phTitle класс */>
+        У этого пользователя ещё нет объявлений
+      </Box>
+    ) : (
+      <Box className="phTitle dark" /* не нашел  phTitle класс */>
+        У этого пользователя нет завершенных объявлений
+      </Box>
+    )
+  ) : (
+    <Box className="phTitle dark" /* не нашел  phTitle класс */>
+      У этого пользователя ещё нет объявлений
+    </Box>
+  );
   return (
-    <Box className="clientPage__container_bottom">
-      <Box className="clientPage__container_content">
-        <Box className="phOffers">
-          {user ? (
-            user == 1 ? (
-              <Box className="phTitle dark">
-                У этого пользователя ещё нет объявлений
-              </Box>
-            ) : (
-              <Box className="phTitle dark">
-                У этого пользователя нет завершенных объявлений
-              </Box>
-            )
-          ) : (
-            <Box className="phTitle dark">
-              У этого пользователя ещё нет объявлений
-            </Box>
-          )}
-          <Box className="phContent">
-            <Box className="phCard">
+    <Box className={classes.clientPage__container_bottom}>
+      <Box className={classes.clientPage__container_content}>
+        <Box className={classes.phOffers}>
+          {userBlock}
+          <Box className={classes.phContent}>
+            <Box className={classes.phCard}>
               <Box className={classes.phCardPhoto} />
-              <Box className="phCardTitle" />
-              <Box className="phCardSubtitle" />
+              <Box className={classes.phCardTitle} />
+              <Box className={classes.phCardSubtitle} />
             </Box>
-            <Box className="phCard">
+            <Box className={classes.phCard}>
               <Box className={classes.phCardPhoto} />
-              <Box className="phCardTitle" />
-              <Box className="phCardSubtitle" />
+              <Box className={classes.phCardTitle} />
+              <Box className={classes.phCardSubtitle} />
             </Box>
-            <Box className="phCard">
+            <Box className={classes.phCard}>
               <Box className={classes.phCardPhoto} />
-              <Box className="phCardTitle" />
-              <Box className="phCardSubtitle" />
+              <Box className={classes.phCardTitle} />
+              <Box className={classes.phCardSubtitle} />
             </Box>
           </Box>
         </Box>
