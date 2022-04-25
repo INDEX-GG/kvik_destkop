f4b25b88dce62cefe05d1dd50ab8e89e99e22645
Evgeny_Kiselev <mor-cre@mail.ru>
2022-04-25 17:12:15 +0500

    форматирование иконки

--- src/UI/UIicon/EyeIcon.js
+++ src/UI/UIicon/EyeIcon.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const EyeIcon = () => {
-    return (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="12" cy="12" r="4" stroke="#5A5A5A" strokeWidth="2"/>
-            <path d="M12 5C5.75 5 2.72917 9.56944 2 12C2.72917 14.2361 5.75 19 12 19C18.25 19 21.2708 14.2361 22 12C21.2708 9.56944 18.25 5 12 5Z" stroke="#5A5A5A" stroke-width="2"/>
-        </svg>
-    );
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="12" cy="12" r="4" stroke="#5A5A5A" strokeWidth="2" />
+      <path
+        d="M12 5C5.75 5 2.72917 9.56944 2 12C2.72917 14.2361 5.75 19 12 19C18.25 19 21.2708 14.2361 22 12C21.2708 9.56944 18.25 5 12 5Z"
+        stroke="#5A5A5A"
+        stroke-width="2"
+      />
+    </svg>
+  );
 };
 
 export default EyeIcon;
