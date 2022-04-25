fdde388c1bc5faa95d8d9bcbdb72a480e3e430f3
Evgeny_Kiselev <mor-cre@mail.ru>
2022-04-25 15:06:49 +0500

    форматирование custombuttonui

--- src/UI/UIcomponent/CustomButtonUI/CustomButtonUI.js
+++ src/UI/UIcomponent/CustomButtonUI/CustomButtonUI.js
@@ -1,34 +1,36 @@
 import React from "react";
-import {Button} from "@material-ui/core";
-import {useCustomButtonUIStyles} from './style';
+import { Button } from "@material-ui/core";
+import { useCustomButtonUIStyles } from "./style";
 
-const CustomButtonUI = (
-    {
-        children,
-        color='default',
-        type = 'button',
-        onClick = () => null,
-        customRoot = null,
-        customCircle = null,
-    }
-) => {
+const CustomButtonUI = ({
+  children,
+  color = "default",
+  type = "button",
+  onClick = () => null,
+  customRoot = null,
+  customCircle = null,
+}) => {
+  const classes = useCustomButtonUIStyles();
+  const classesButton = customRoot
+    ? `${customRoot} ${classes.button}`
+    : classes.button;
+  const classesCircle = customCircle
+    ? `${customCircle} ${classes.circle}`
+    : classes.circle;
+  const classesRoot =
+    type === "circle" ? `${classesButton} ${classesCircle}` : classesButton;
 
-    const classes = useCustomButtonUIStyles()
-    const classesButton = customRoot ? `${customRoot} ${classes.button}` : classes.button;
-    const classesCircle = customCircle ? `${customCircle} ${classes.circle}` : classes.circle;
-    const classesRoot = type === 'circle' ? `${classesButton} ${classesCircle}` : classesButton;
-
-    return (
-        <Button
-            color={color}
-            onClick={onClick}
-            classes={{
-                root: `${classes.default} ${classesRoot}`
-            }}
-        >
-            {children}
-        </Button>
-    )
-}
+  return (
+    <Button
+      color={color}
+      onClick={onClick}
+      classes={{
+        root: `${classes.default} ${classesRoot}`,
+      }}
+    >
+      {children}
+    </Button>
+  );
+};
 
 export default React.memo(CustomButtonUI);
