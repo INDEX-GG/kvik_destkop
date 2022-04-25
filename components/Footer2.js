1611926b94849bb151c93e3cdb82804be0a5fc44
Evgeny_Kiselev <mor-cre@mail.ru>
2022-04-25 14:50:25 +0500

    форматирование и добавление в footer2

--- components/Footer2.js
+++ components/Footer2.js
@@ -1,8 +1,7 @@
 import { useMedia } from "../hooks/useMedia";
-
+import CustomLinkUI from "src/UI/UIcomponent/CustomLinkUI/CustomLinkUI";
 function Footer2() {
-
-  const { matchesMobile, matchesTablet, matchesCustom1024 } = useMedia()
+  const { matchesMobile, matchesTablet, matchesCustom1024 } = useMedia();
 
   return (
     <div className="main__block__footer">
@@ -10,30 +9,60 @@ function Footer2() {
         <>
           <p>Скачать приложение</p>
           <div className="block__links__download">
-            <a className="ios_links__download" href=""></a>
-            <a className="android_links__download" href=""></a>
+            <a className="ios_links__download" href="" />
+            <a className="android_links__download" href="" />
           </div>
         </>
-      ) : <div style={{ height: "63px" }}></div>}
+      ) : (
+        <div style={{ height: "63px" }} />
+      )}
 
       <div className="footer__bottom">
         <div className="socialNetworksFooter2">
-					{/*<a href="https://www.instagram.com/kvik_baraholka/" className="instagramLinkFooter" target="_blank" rel="noreferrer">*/}
-            {/*<div className="instagramLinkIcon"></div>*/}
+          {/*<a href="https://www.instagram.com/kvik_baraholka/" className="instagramLinkFooter" target="_blank" rel="noreferrer">*/}
+          {/*<div className="instagramLinkIcon"></div>*/}
           {/*</a>*/}
-					<a href="https://vk.com/1kvik_ru" className="vkLinkFooter" target="_blank" rel="noreferrer">
-            <div className="vkLinkIcon"></div>
+          <a
+            href="https://vk.com/1kvik_ru"
+            className="vkLinkFooter"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="vkLinkIcon" />
           </a>
-					{/*<a href="https://www.facebook.com/Kvik-Барахолкачелябинск-107509641798965" className="fbLinkFooter" target="_blank" rel="noreferrer">*/}
-            {/*<div className="fbLinkIcon" ></div>*/}
+          {/*<a href="https://www.facebook.com/Kvik-Барахолкачелябинск-107509641798965" className="fbLinkFooter" target="_blank" rel="noreferrer">*/}
+          {/*<div className="fbLinkIcon" ></div>*/}
           {/*</a>*/}
         </div>
         <div className="linkContainerFooter2">
-          <a className='aboutLinkFooter2' href="/about">О компании</a>
-          <a className='contactsLinkFooter2' href="/contacts">Контакты</a>
-          <a className='offerLinkFooter2' href='/offer'>Оферта</a>
+          <CustomLinkUI
+            defaultColor={false}
+            customRoot="aboutLinkFooter2"
+            href="/about"
+          >
+            О компании
+          </CustomLinkUI>
+          <CustomLinkUI
+            defaultColor={false}
+            customRoot="contactsLinkFooter2"
+            href="/contacts"
+          >
+            Контакты
+          </CustomLinkUI>
+          <CustomLinkUI
+            defaultColor={false}
+            customRoot="offerLinkFooter2"
+            href="/offer"
+          >
+            Оферта
+          </CustomLinkUI>
         </div>
-        <div className='indexTitleFooter2'> <a target="_blank" href="https://lndex.ru" rel="noopener noreferrer">Разработано студией INDEX</a></div>
+        <div className="indexTitleFooter2">
+          {" "}
+          <a target="_blank" href="https://lndex.ru" rel="noopener noreferrer">
+            Разработано студией INDEX
+          </a>
+        </div>
       </div>
     </div>
   );
