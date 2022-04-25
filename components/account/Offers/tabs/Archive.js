9d2acaa15df095b1a479b5d60fa8b944395b78e2
Evgeny_Kiselev <mor-cre@mail.ru>
2022-04-25 17:32:10 +0500

    форматирование archive

--- components/account/Offers/tabs/Archive.js
+++ components/account/Offers/tabs/Archive.js
@@ -2,56 +2,56 @@ import React, { useState, useEffect } from "react";
 import EmptyPlaceholder from "../../../EmptyPlaceholder";
 import OfferModal from "../../../OfferModal";
 import { useOfferAccount } from "../../../../lib/Context/OfferAccountCTX";
-import { Checkbox, makeStyles, Dialog  } from "@material-ui/core";
-import FiberManualRecordOutlinedIcon from '@material-ui/icons/FiberManualRecordOutlined';
-import FiberManualRecordSharpIcon from '@material-ui/icons/FiberManualRecordSharp';
+import { Checkbox, makeStyles, Dialog } from "@material-ui/core";
+import FiberManualRecordOutlinedIcon from "@material-ui/icons/FiberManualRecordOutlined";
+import FiberManualRecordSharpIcon from "@material-ui/icons/FiberManualRecordSharp";
 import OfferWaitPlaceHolder from "../../../placeHolders/OfferPlaceHolder/OfferWaitPlaceHolder/OfferWaitPlaceHolder";
-import {Box, Grid} from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import ArchiveIco from "./ArchiveIco/ArchiveIco";
 import OfferCard from "../card/OfferCard";
 
 const useStyles = makeStyles((theme) => ({
-	check: {
-		padding: "0px",
-		background: theme.palette.secondary.main,
-		width: "14px",
-		height: "14px",
+  check: {
+    padding: "0px",
+    background: theme.palette.secondary.main,
+    width: "14px",
+    height: "14px",
 
-		"&:hover": {
-			background: theme.palette.secondary.main,
-		},
-	},
-	btn__delete: {
-		marginLeft: "12px",
-		background: "none",
-		color: theme.palette.grey[200],
-		cursor: "pointer",
-		transition: "all 200ms ease-in-out",
+    "&:hover": {
+      background: theme.palette.secondary.main,
+    },
+  },
+  btn__delete: {
+    marginLeft: "12px",
+    background: "none",
+    color: theme.palette.grey[200],
+    cursor: "pointer",
+    transition: "all 200ms ease-in-out",
 
-		"&:hover": {
-			transition: "all 200ms ease-in-out",
-			textDecoration: "underline",
-		},
-	},
-	btn__publish: {
-		marginLeft: "12px",
-		background: "none",
-		color: theme.palette.grey[200],
-		cursor: "pointer",
-		transition: "all 200ms ease-in-out",
+    "&:hover": {
+      transition: "all 200ms ease-in-out",
+      textDecoration: "underline",
+    },
+  },
+  btn__publish: {
+    marginLeft: "12px",
+    background: "none",
+    color: theme.palette.grey[200],
+    cursor: "pointer",
+    transition: "all 200ms ease-in-out",
 
-		"&:hover": {
-			transition: "all 200ms ease-in-out",
-			textDecoration: "underline",
-		},
-	},
-	archive: {
-		display: "flex",
-		justifyContent: "center",
-	},
-	text: {
-		fontSize: "18px"
-	},
+    "&:hover": {
+      transition: "all 200ms ease-in-out",
+      textDecoration: "underline",
+    },
+  },
+  archive: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: "18px",
+  },
 }));
 
 /**
@@ -62,158 +62,181 @@ const useStyles = makeStyles((theme) => ({
 /**
  * @param {ArchiveProps} props
  */
-function Archive({offers}) {
-	const classes = useStyles();
-	const { page, setPage, totalPosts, page_limit } = useOfferAccount()
-	const [isFirstRender, setIsFirstRender] = useState(true)
-	const [openOfferModal, setOpenOfferModal] = useState(false);
-	const [check, setCheck] = useState(false);
-	const [offerId, setOfferId] = useState([]);
-	const [offerData, setOfferData] = useState([]);
-	const [buttonId, setButtonId] = useState('');
-	const offersLength = offers.length
+function Archive({ offers }) {
+  const classes = useStyles();
+  const { page, setPage, totalPosts, page_limit } = useOfferAccount();
+  const [isFirstRender, setIsFirstRender] = useState(true);
+  const [openOfferModal, setOpenOfferModal] = useState(false);
+  const [check, setCheck] = useState(false);
+  const [offerId, setOfferId] = useState([]);
+  const [offerData, setOfferData] = useState([]);
+  const [buttonId, setButtonId] = useState("");
+  const offersLength = offers.length;
 
-	const cleanAll = () => {
-		setCheck(false);
-		setOfferId([]);
-		setOfferData([]);
-	}
+  const cleanAll = () => {
+    setCheck(false);
+    setOfferId([]);
+    setOfferData([]);
+  };
 
-	function getChildCheck ({id, isCheck}) {
-		setOfferId( isCheck ? prev => [...prev, id] : prev => prev.filter( item => item !== id) );
-		setOfferData( isCheck ? prev => [...prev, offers.filter( item => item.id === id )[0]] : prev => prev.filter( item => item.id !== id) );
-	}
+  function getChildCheck({ id, isCheck }) {
+    setOfferId(
+      isCheck
+        ? (prev) => [...prev, id]
+        : (prev) => prev.filter((item) => item !== id)
+    );
+    setOfferData(
+      isCheck
+        ? (prev) => [...prev, offers.filter((item) => item.id === id)[0]]
+        : (prev) => prev.filter((item) => item.id !== id)
+    );
+  }
 
-	// условие setCheck(true) ставит галочку при отрисовки страницы
-	useEffect( () => {
-		offerId.length === offers.length
-			? check
-				? null
-				: setCheck(false)
-			: check===false
-				? null
-				: setCheck(true)
-	}, [offerId]);
+  // условие setCheck(true) ставит галочку при отрисовки страницы
+  useEffect(() => {
+    offerId.length === offers.length
+      ? check
+        ? null
+        : setCheck(false)
+      : check === false
+      ? null
+      : setCheck(true);
+  }, [offerId]);
 
-	useEffect(()=> {
-		if(isFirstRender) {
-			setIsFirstRender(false)
-			return
-		}
-        document.addEventListener('scroll', scrollHandler )
-        return ()=>{
-            document.removeEventListener('scroll', scrollHandler )
-        }
-    }, [totalPosts, isFirstRender] )
+  useEffect(() => {
+    if (isFirstRender) {
+      setIsFirstRender(false);
+      return;
+    }
+    document.addEventListener("scroll", scrollHandler);
+    return () => {
+      document.removeEventListener("scroll", scrollHandler);
+    };
+  }, [totalPosts, isFirstRender]);
 
   // pageNumber - переменная для сохранения значения. (сделана из-за того, что для функции scrollHandler page всегда равна первому значению)
-	let pageNumber = page
-	function scrollHandler (e) {
+  let pageNumber = page;
+  function scrollHandler(e) {
     // высчитываем высоту отступа между скролом и низом страницы
-		const pixelsFromBottom = (e.target.documentElement.scrollHeight - e.target.documentElement.scrollTop)-window.innerHeight;
-		const maxPossiblePage = Math.ceil(totalPosts.archive / page_limit);
-		if(maxPossiblePage <= pageNumber && !isFirstRender) {
-			return
-		}
-		// если находится нужная нам высота, обновляем страницу для повторого запроса.
-		if(pixelsFromBottom <= 200){
-			setPage(pageNumber + 1)
-			pageNumber += 1
-		}
-	}
+    const pixelsFromBottom =
+      e.target.documentElement.scrollHeight -
+      e.target.documentElement.scrollTop -
+      window.innerHeight;
+    const maxPossiblePage = Math.ceil(totalPosts.archive / page_limit);
+    if (maxPossiblePage <= pageNumber && !isFirstRender) {
+      return;
+    }
+    // если находится нужная нам высота, обновляем страницу для повторого запроса.
+    if (pixelsFromBottom <= 200) {
+      setPage(pageNumber + 1);
+      pageNumber += 1;
+    }
+  }
 
-	if (offers.length === 0) {
-		return (
-			<>
-				<Grid container spacing={2}>
-					<Grid item xs={12}>
-						{!offers
-							? <OfferWaitPlaceHolder/>
-							: <EmptyPlaceholder
-								title='Здесь будет ваш архив объявлений'
-								subtitle=''
-								className={classes.text}
-							/>
-						}
-					</Grid>
-					<Grid item xs={12}>
-						<Box className={classes.archive}>
-							<ArchiveIco />
-						</Box>
-					</Grid>
-				</Grid>
+  if (offers.length === 0) {
+    return (
+      <>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            {!offers ? (
+              <OfferWaitPlaceHolder />
+            ) : (
+              <EmptyPlaceholder
+                title="Здесь будет ваш архив объявлений"
+                subtitle=""
+                className={classes.text}
+              />
+            )}
+          </Grid>
+          <Grid item xs={12}>
+            <Box className={classes.archive}>
+              <ArchiveIco />
+            </Box>
+          </Grid>
+        </Grid>
+      </>
+    );
+  }
 
+  /* Модальное окно */
+  function pushCheck(e) {
+    setButtonId(e.target.id);
+    setOpenOfferModal(!openOfferModal);
+  }
 
-			</>
-		);
-	}
-
-	/* Модальное окно */
-	function pushCheck(e) {
-		setButtonId(e.target.id)
-		setOpenOfferModal(!openOfferModal);
-	}
-
-	return (
-		<>
-			{!offers
-			?
-			<OfferWaitPlaceHolder/>
-			:
-			<div className="clientPage__container_bottom">
-				{offers.length > 1 && <div className="clientPage__container_nav__radio">
-					<Checkbox
-						className={classes.check}
-						color="primary"
-						value=""
-						icon={<FiberManualRecordOutlinedIcon/>}
-						checkedIcon={<FiberManualRecordSharpIcon/>}
-						onChange={(e) => {
-							e.target.checked === false ? cleanAll() : setCheck(e.target.checked);
-						}}
-						checked={check}
-					/>
-					{/* пока что закоментировано так как активировать можно только по-одному объявлению */}
-					{/* <button id='001' className={classes.btn__publish} onClick={(e) => {
+  return (
+    <>
+      {!offers ? (
+        <OfferWaitPlaceHolder />
+      ) : (
+        <div className="clientPage__container_bottom">
+          {offers.length > 1 && (
+            <div className="clientPage__container_nav__radio">
+              <Checkbox
+                className={classes.check}
+                color="primary"
+                value=""
+                icon={<FiberManualRecordOutlinedIcon />}
+                checkedIcon={<FiberManualRecordSharpIcon />}
+                onChange={(e) => {
+                  e.target.checked === false
+                    ? cleanAll()
+                    : setCheck(e.target.checked);
+                }}
+                checked={check}
+              />
+              {/* пока что закоментировано так как активировать можно только по-одному объявлению */}
+              {/* <button id='001' className={classes.btn__publish} onClick={(e) => {
 						offerData.length > 0 ? pushCheck(e) : null
 					}}>
 						Активировать
 					</button> */}
-					<button id='002' className={classes.btn__delete} onClick={(e) => {
-						offerData.length > 0 ? pushCheck(e) : null
-					}}>
-						Удалить
-					</button>
-				</div>}
-				<div className="clientPage__container_content">
-					{offers?.map((offer) => {
-						return (
-							<OfferCard
-								key={offer.id}
-								offer={offer}
-								typeTab='archiveTab'
-								typeButton={'001'}
-								parentCheck={check}
-								getChildCheck={getChildCheck}
-								parentUnpublishForm={openOfferModal}
-								allDataCheck={offerId}
-								offersLength={offersLength}
-							/>
-						);
-					})}
-				</div>
-			</div>}
-			<Dialog open={openOfferModal} onClose={() => setOpenOfferModal(!openOfferModal)} fullWidth maxWidth="md">
-				<OfferModal
-					offerId={offerId}
-					offerData={offerData}
-					openOfferModal={openOfferModal}
-					setOpenOfferModal={setOpenOfferModal}
-					buttonId={buttonId}
-					cleanAll={cleanAll}
-				/>
-			</Dialog>
-		</>
-	);
+              <button
+                id="002"
+                className={classes.btn__delete}
+                onClick={(e) => {
+                  offerData.length > 0 ? pushCheck(e) : null;
+                }}
+              >
+                Удалить
+              </button>
+            </div>
+          )}
+          <div className="clientPage__container_content">
+            {offers?.map((offer) => {
+              return (
+                <OfferCard
+                  key={offer.id}
+                  offer={offer}
+                  typeTab="archiveTab"
+                  typeButton={"001"}
+                  parentCheck={check}
+                  getChildCheck={getChildCheck}
+                  parentUnpublishForm={openOfferModal}
+                  allDataCheck={offerId}
+                  offersLength={offersLength}
+                />
+              );
+            })}
+          </div>
+        </div>
+      )}
+      <Dialog
+        open={openOfferModal}
+        onClose={() => setOpenOfferModal(!openOfferModal)}
+        fullWidth
+        maxWidth="md"
+      >
+        <OfferModal
+          offerId={offerId}
+          offerData={offerData}
+          openOfferModal={openOfferModal}
+          setOpenOfferModal={setOpenOfferModal}
+          buttonId={buttonId}
+          cleanAll={cleanAll}
+        />
+      </Dialog>
+    </>
+  );
 }
 export default Archive;
