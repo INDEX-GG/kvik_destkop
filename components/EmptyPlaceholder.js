d09ab99c66763bc90c5126f34b4f8cb1ee9408e9
Evgeny_Kiselev <mor-cre@mail.ru>
2022-04-25 16:11:29 +0500

    форматирование emptyplaceholder

--- components/EmptyPlaceholder.js
+++ components/EmptyPlaceholder.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 /**
  * @typedef EmptyPlaceholderProps
@@ -11,22 +11,36 @@ import React from 'react';
  */
 
 /**
- * @param {EmptyPlaceholderProps} props 
+ * @param {EmptyPlaceholderProps} props
  */
-const EmptyPlaceholder = ({title, subtitle, img, imgAlt, className = undefined, customClass = ''}) => {
-	const titleClass = ["notInf__title", className && className].join(" ");
+const EmptyPlaceholder = ({
+  title,
+  subtitle,
+  img,
+  imgAlt,
+  className = undefined,
+  customClass = "",
+}) => {
+  const titleClass = ["notInf__title", className && className].join(" ");
 
-	return (
-		<div className="clientPage__container_bottom">
-			<div className="clientPage__container_content">
-				<div className="notInfContainer">
-					{title && <div className={titleClass}>{title}</div>}
-					{subtitle && <p className="notInf__subtitle">{subtitle}</p>}
-					{img && <img  className={`notInf__img-${customClass}`} style={{maxWidth: "650px"}} src={img} alt={imgAlt}/>}
-				</div>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="clientPage__container_bottom">
+      <div className="clientPage__container_content">
+        <div className="notInfContainer">
+          {title && <div className={titleClass}>{title}</div>}
+          {subtitle && <p className="notInf__subtitle">{subtitle}</p>}
+          {img && (
+            <img
+              className={`notInf__img-${customClass}`}
+              style={{ maxWidth: "650px" }}
+              src={img}
+              alt={imgAlt}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default EmptyPlaceholder;
