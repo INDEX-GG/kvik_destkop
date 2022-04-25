3f220217c75daec9ec5d254f991ae44e37a03932
Evgeny_Kiselev <mor-cre@mail.ru>
2022-04-25 15:57:02 +0500

    форматирование offercardpart

--- components/account/Offers/card/OfferCardPart/imgCard.js
+++ components/account/Offers/card/OfferCardPart/imgCard.js
@@ -1,32 +1,35 @@
-import React, {useMemo} from "react";
+import React, { useMemo } from "react";
 
 // import { STATIC_URL } from '#lib/constants'
 // import { photos2arr } from '#lib/services'
 
 const Image = (props) => {
+  const offerLabelByType = {
+    banned: "Отклонено",
+    time_limit: "Истек срок размещения",
+  };
 
-    const offerLabelByType = {
-        banned: 'Отклонено',
-        time_limit: 'Истек срок размещения',
-    }
+  const labelImage = Object.prototype.hasOwnProperty.call(
+    offerLabelByType,
+    props.offer?.status
+  )
+    ? offerLabelByType[(props.offer?.status)]
+    : "";
 
-    const labelImage = Object.prototype.hasOwnProperty.call(offerLabelByType, props.offer?.status) ? offerLabelByType[props.offer?.status] : '';
+  const photos = useMemo(
+    // () => Array.isArray(props.offer.photo) ? props.offer.photo || photos2arr(props.offer.photo)?.map( img => `${STATIC_URL}/${img}`)
+    () => props?.offer?.photo
+  );
 
-    const photos = useMemo(
-        // () => Array.isArray(props.offer.photo) ? props.offer.photo || photos2arr(props.offer.photo)?.map( img => `${STATIC_URL}/${img}`)
-        () => props?.offer?.photo
-    )
+  return (
+    <div className={props.classes.offer__image}>
+      {photos &&
+        photos?.slice(0, 1)?.map((imgs, i) => {
+          return <img className={props.classes.image} key={i} src={imgs} />;
+        })}
+      {props.isWaith && <p className={props.classes.pos_abs}>{labelImage}</p>}
+    </div>
+  );
+};
 
-    return (
-        <div className={props.classes.offer__image}>
-            {photos && photos?.slice(0, 1)?.map((imgs, i) => {
-                return <img className={props.classes.image} key={i} src={imgs} />;
-            })}
-            {props.isWaith && (
-                <p className={props.classes.pos_abs}>{labelImage}</p>
-            )}
-        </div>
-    )
-}
-
-export default React.memo(Image)
+export default React.memo(Image);
