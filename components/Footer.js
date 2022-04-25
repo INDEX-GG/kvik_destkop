073266278e32ca1a1dfb973c13f1861a6199a3ff
Evgeny_Kiselev <mor-cre@mail.ru>
2022-04-25 14:57:19 +0500

    изменил footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,38 +1,69 @@
+import CustomLinkUI from "src/UI/UIcomponent/CustomLinkUI/CustomLinkUI";
+
 const Footer = function() {
   return (
     <div className="footer">
-      <div className='footerLeftVoid'></div>
-      <div className='footerMainContainer'>
+      <div className="footerLeftVoid" />
+      <div className="footerMainContainer">
         <div className="linkContainerFooter">
-          <a className='aboutLinkFooter' href="/about">О компании</a>
-          <a className='contactsLinkFooter' href="/contacts">Контакты</a>
-          <a className='offerLinkFooter' href='/offer'>Оферта</a>
+          <CustomLinkUI
+            defaultColor={false}
+            customRoot="aboutLinkFooter"
+            href="/about"
+          >
+            О компании
+          </CustomLinkUI>
+          <CustomLinkUI
+            defaultColor={false}
+            customRoot="contactsLinkFooter"
+            href="/contacts"
+          >
+            Контакты
+          </CustomLinkUI>
+          <CustomLinkUI
+            defaultColor={false}
+            customRoot="offerLinkFooter"
+            href="/offer"
+          >
+            Оферта
+          </CustomLinkUI>
         </div>
         <div className="socialNetworksFooter">
-					{/*<a href="https://www.instagram.com/kvik_baraholka/" className="instagramLinkFooter" target="_blank" rel="noreferrer">*/}
-            {/*<div className="instagramLinkIcon"></div>*/}
+          {/*<a href="https://www.instagram.com/kvik_baraholka/" className="instagramLinkFooter" target="_blank" rel="noreferrer">*/}
+          {/*<div className="instagramLinkIcon"></div>*/}
           {/*</a>*/}
-					<a href="https://vk.com/1kvik_ru" className="vkLinkFooter" target="_blank" rel="noreferrer">
-            <div className="vkLinkIcon"></div>
+          <a
+            href="https://vk.com/1kvik_ru"
+            className="vkLinkFooter"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="vkLinkIcon" />
           </a>
-					{/*<a href="https://www.facebook.com/Kvik-Барахолкачелябинск-107509641798965" className="fbLinkFooter" target="_blank" rel="noreferrer">*/}
-            {/*<div className="fbLinkIcon" ></div>*/}
+          {/*<a href="https://www.facebook.com/Kvik-Барахолкачелябинск-107509641798965" className="fbLinkFooter" target="_blank" rel="noreferrer">*/}
+          {/*<div className="fbLinkIcon" ></div>*/}
           {/*</a>*/}
         </div>
         <div className="rightContainer">
           <div className="links__download">
-            <a href="https://play.google.com/store" className="androidDownloadLinkFooter">
-              <div className="androidDownloadLinkIconFooter"></div>
+            <a
+              href="https://play.google.com/store"
+              className="androidDownloadLinkFooter"
+            >
+              <div className="androidDownloadLinkIconFooter" />
             </a>
             <a href="https:/facebook.com" className="iosDownloadLinkFooter">
-              <div className="iosDownloadLinkIconFooter"></div>
+              <div className="iosDownloadLinkIconFooter" />
             </a>
           </div>
         </div>
-        <div className='indexTitleFooter'> <a href="https://lndex.ru"> Разработано студией INDEX</a></div>
+        <div className="indexTitleFooter">
+          {" "}
+          <a href="https://lndex.ru"> Разработано студией INDEX</a>
+        </div>
       </div>
-      <div className='footerRightVoid'></div>
+      <div className="footerRightVoid" />
     </div>
   );
-}
+};
 export default Footer;
