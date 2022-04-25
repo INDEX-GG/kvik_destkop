cacd4d687684631fc07af1f2ed5fd21ebe477242
Evgeny_Kiselev <mor-cre@mail.ru>
2022-04-25 15:27:14 +0500

    форматирование

--- components/account/Favorites/sellers/SellersItem.js
+++ components/account/Favorites/sellers/SellersItem.js
@@ -1,57 +1,57 @@
-import React, {useState, useMemo} from 'react';
-import {STATIC_URL} from "../../../../lib/constants";
-import {ellipsis, ToRubles} from "../../../../lib/services";
-import {useRouter} from "next/router";
-import {useAuth} from "../../../../lib/Context/AuthCTX";
-import {getTokenDataByPost} from "../../../../lib/fetch";
-import { useMedia } from '../../../../hooks/useMedia';
+import React, { useState, useMemo } from "react";
+import { STATIC_URL } from "../../../../lib/constants";
+import { ellipsis, ToRubles } from "../../../../lib/services";
+import { useRouter } from "next/router";
+import { useAuth } from "../../../../lib/Context/AuthCTX";
+import { getTokenDataByPost } from "../../../../lib/fetch";
+import { useMedia } from "../../../../hooks/useMedia";
 
 // swiper объявлений
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { Navigation } from "swiper/core";
 SwiperCore.use([Navigation]);
 
-
-const SellersItem = ({seller}) => {
-  const {id, token} = useAuth();
-  const [subscribe, setSubscribe] = useState(true)
+const SellersItem = ({ seller }) => {
+  const { id, token } = useAuth();
+  const [subscribe, setSubscribe] = useState(true);
   const router = useRouter();
-	const { matchesMobile } = useMedia();
+  const { matchesMobile } = useMedia();
 
   function subscribeUser(id, sellerID) {
-
-    getTokenDataByPost("/api/subscribe", {
-      user_id: id,
-      subscribe: !subscribe ? [sellerID] : [],
-      unsubscribe: subscribe ? [sellerID] : [],
-    }, token)
-    .then(res => console.log(res))
-    .catch(error => console.log(error))
-
+    getTokenDataByPost(
+      "/api/subscribe",
+      {
+        user_id: id,
+        subscribe: !subscribe ? [sellerID] : [],
+        unsubscribe: subscribe ? [sellerID] : [],
+      },
+      token
+    )
+      .then((res) => console.log(res))
+      .catch((error) => console.log(error));
   }
-
 
   const hanleClick = () => {
-    setSubscribe(!subscribe)
+    setSubscribe(!subscribe);
     if (id && seller.id) {
-      subscribeUser(id, seller.id)
+      subscribeUser(id, seller.id);
     }
-  }
+  };
 
   /**
    * Render Seller productOld item of array
    * @param {offer} (Object)
    * @returns JSX
    */
-  const SellerProductItem = ({offer}) => {
+  const SellerProductItem = ({ offer }) => {
     return (
       <a href={`/product/${offer.id}`} className="sellersOffer">
-        <img src={`${STATIC_URL}/${JSON.parse(offer.photo)?.photos[0]}`}/>
+        <img src={`${STATIC_URL}/${JSON.parse(offer.photo)?.photos[0]}`} />
         <div>{ellipsis(ToRubles(offer.price), 15)}</div>
         <div>{ellipsis(offer.title, 10)}</div>
       </a>
-    )
-  }
+    );
+  };
 
   /**
    * Render seller.products one line layout
@@ -61,62 +61,69 @@ const SellersItem = ({seller}) => {
    * @returns JSX
    */
   const SellersProducts = useMemo(
-    () => ({seller}) => {
-    return (
-      matchesMobile
-      ? (
-        seller?.user_products_count >= 3
-        ? (
-          <Swiper
-            navigation={true}
-            slidesPerView='auto'
-          >
+    () => ({ seller }) => {
+      return matchesMobile ? (
+        seller?.user_products_count >= 3 ? (
+          <Swiper navigation={true} slidesPerView="auto">
+            {seller?.user_products.map((offerProduct) => {
+              return (
+                <SwiperSlide key={offerProduct.id}>
+                  <SellerProductItem offer={offerProduct} />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        ) : (
+          seller?.user_products.map((offerProduct) => (
+            <SellerProductItem key={offerProduct.id} offer={offerProduct} />
+          ))
+        )
+      ) : seller?.user_products_count >= 7 ? (
+        <Swiper navigation={true} slidesPerView="auto">
           {seller?.user_products.map((offerProduct) => {
             return (
               <SwiperSlide key={offerProduct.id}>
-                <SellerProductItem  offer={offerProduct} />
+                <SellerProductItem offer={offerProduct} />
               </SwiperSlide>
             );
           })}
         </Swiper>
-        ): seller?.user_products.map((offerProduct) => <SellerProductItem key={offerProduct.id} offer={offerProduct} />)
       ) : (
-        seller?.user_products_count >= 7
-        ? (
-          <Swiper
-            navigation={true}
-            slidesPerView='auto'
-          >
-          {seller?.user_products.map((offerProduct) => {
-            return (
-              <SwiperSlide key={offerProduct.id}>
-                <SellerProductItem  offer={offerProduct} />
-              </SwiperSlide>
-            );
-          })}
-        </Swiper>
-        ) : seller?.user_products.map((offerProduct) => <SellerProductItem key={offerProduct.id} offer={offerProduct} />)
-      )
-    )
-  }, [seller, matchesMobile])
+        seller?.user_products.map((offerProduct) => (
+          <SellerProductItem key={offerProduct.id} offer={offerProduct} />
+        ))
+      );
+    },
+    [seller, matchesMobile]
+  );
 
   return (
     <div key={seller.id} className="sellersContainer">
       <div className="sellersUser">
-        <div onClick={() => {
-          router.push(`/user/${seller.id}`)
-        }} className="sellersUserBlock">
-          <img src={`${STATIC_URL}/${seller.userPhoto}`}/>
+        <div
+          onClick={() => {
+            router.push(`/user/${seller.id}`);
+          }}
+          className="sellersUserBlock"
+        >
+          <img src={`${STATIC_URL}/${seller.userPhoto}`} />
           <div className="sellersUserInfo">
             <div className="sellersUserName">{seller.name}</div>
-            <div className="sellersOffersCount light">{seller?.user_products_count} объявлений</div>
+            <div className="sellersOffersCount light">
+              {seller?.user_products_count} объявлений
+            </div>
           </div>
         </div>
-        <button onClick={() => {
-          hanleClick()
-          // sellerSub(58, seller.id)
-          // changeSubscribe(index)
-        }} className="buttonGrey">{subscribe ? "Отписаться" : "Подписаться"}</button>
+        <button
+          onClick={() => {
+            hanleClick();
+            // sellerSub(58, seller.id)
+            // changeSubscribe(index)
+          }}
+          className="buttonGrey"
+        >
+          {subscribe ? "Отписаться" : "Подписаться"}
+        </button>
       </div>
       <div className="sellersOffers">
         <SellersProducts seller={seller} />
